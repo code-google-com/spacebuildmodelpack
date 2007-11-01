87992eff03e0e12ea2e86237074a3fd4274e923f
stijn.sv <stijn.sv@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2007-11-01 10:02:37 +0000

    [Updated] base_livable_module needs resources now to operate. Has wire input to enable/disable the device from giving air, coolant, gravity (default = enabled for the 3 of them)

--- SpacebuildModelPackAlpha2/lua/entities/base_livable_module/init.lua
+++ SpacebuildModelPackAlpha2/lua/entities/base_livable_module/init.lua
@@ -7,9 +7,9 @@ util.PrecacheSound( "common/warning.wav" )
 include('shared.lua')
 
 local Ground = 1 + 0 + 2 + 8 + 32
-local Air_Increment = 20
-local Energy_Increment = 80
-local Coolant_Increment = 20
+local Air_Increment = 2
+local Energy_Increment = 5
+local Coolant_Increment = 2
 
 function ENT:Initialize()
 	self.BaseClass.Initialize(self)
@@ -18,9 +18,13 @@ function ENT:Initialize()
 	SB_Add_Environment(self.Entity, nil, 0, 0, 0, 0)
 	if not (WireAddon == nil) then
 		self.WireDebugName = self.PrintName
-		self.Inputs = Wire_CreateInputs(self.Entity, { "On" })
+		self.Inputs = Wire_CreateInputs(self.Entity, { "On", "Air", "Heat", "Gravity" })
 		self.Outputs = Wire_CreateOutputs(self.Entity, { "On", "Air", "Heat" })
 	end
+	self.giveAir = 1;
+	self.giveHeat = 1;
+	self.giveGravity = 1;
+	self.size = math.Round(self.Entity:BoundingRadius()/48);
 	self.IgnoreClasses = {self.Entity:GetClass()}
 end
 
@@ -47,6 +51,24 @@ end
 function ENT:TriggerInput(iname, value)
 	if (iname == "On") then
 		self:SetActive(value)
+	elseif (iname == "Air") then
+		if value > 0 then
+			self.giveAir = 1
+		else
+			self.giveAir = 0
+		end
+	elseif (iname == "Heat") then
+		if value > 0 then
+			self.giveHeat = 1
+		else
+			self.giveHeat = 0
+		end
+	elseif (iname == "Gravity") then
+		if value > 0 then
+			self.giveGravity = 1
+		else
+			self.giveGravity = 0
+		end
 	end
 end
 
@@ -76,45 +98,93 @@ function ENT:OnRemove()
 end
 
 function ENT:Climate_Control()
-	/*local temperature = self.environment.temperature
+	local temperature = self.environment.temperature
 	local gravity = 0
 	if self.planet then
 		gravity = self.gravity or 0
 	end
-	local atmosphere = self.environment.atmosphere
+	local atmosphere = 0
+	if self.presurized == 1 then
+		atmosphere = 1
+	else
+		atmosphere = self.environment.atmosphere
+	end
 	local habitat = self.environment.habitat
 	if self.environment.inwater == 1 then
 		habitat = 0
 	end
-	if habitat == 0 then
+	if habitat == 0 and self.giveAir == 1 then
 		self.needair = true
 	else
 		self.needair = false
 	end
-	if temperature > FairTemp_Max then
+	if temperature > FairTemp_Max and self.giveHeat == 1 then
 		self.needcoolant = true
 	else
 		self.needcoolant = false
 	end
-	if temperature < FairTemp_Min then
+	if temperature < FairTemp_Min and self.giveHeat == 1 then
 		self.needenergy = true
 	else
 		self.needenergy = false
 	end
 	if self.Active == 1 then
+		self.hasair = false
+		self.hasheat = false
+		self.presurized = 1
 		self.air = RD_GetResourceAmount(self.Entity, "air")
 		self.coolant = RD_GetResourceAmount(self.Entity, "coolant")
 		self.energy = RD_GetResourceAmount(self.Entity, "energy")
+		if self.energy > 0 then
+			if self.needair then
+				if self.air > 0 then
+					RD_ConsumeResource(self.Entity, "air", self.size * Air_Increment)
+					self.hasair = true
+				else
+					self.hasair = false
+				end
+			end
+			if self.needcoolant then
+				if self.coolant > 0 then
+					RD_ConsumeResource(self.Entity, "coolant", self.size * Coolant_Increment)
+					self.hasheat = true
+				end
+			end
+			if self.needenergy then
+				if self.energy > 0 then
+					RD_ConsumeResource(self.Entity, "energy", self.size * Coolant_Increment)
+					self.hasheat = true
+				end
+			end
+			if self.giveGravity == 1 and gravity != 1 then
+				RD_ConsumeResource(self.Entity, "energy", self.size * Coolant_Increment)
+				gravity = 1
+			end	
+			RD_ConsumeResource(self.Entity, "energy", self.size * Energy_Increment)	
+			if self.hasair then
+				habitat = 1
+			end
+			if self.hasheat then
+				temperature = 288
+				if self.heat != 0
+					self.heat = 0
+				end
+			end
+			if not (WireAddon == nil) then Wire_TriggerOutput(self.Entity, "Air", self.hasair) end
+			if not (WireAddon == nil) then Wire_TriggerOutput(self.Entity, "Heat", self.hasheat) end
+		else
+			self:TurnOff()
+		end
+	else
+		self.presurized = 0			
 	end
-	SB_Update_Environment(self.num, nil, gravity, habitat, atmosphere, temperature)*/
-	SB_Update_Environment(self.num, nil, 1, 1, 1, 288)
+	SB_Update_Environment(self.num, nil, gravity, habitat, atmosphere, temperature)
+	//SB_Update_Environment(self.num, nil, 1, 1, 1, 288)
 end
 
 function ENT:Think()
 	self.BaseClass.Think(self)
-	
 	self:Climate_Control()
-	
 	self.Entity:NextThink(CurTime() + 1)
 	return true
 end
