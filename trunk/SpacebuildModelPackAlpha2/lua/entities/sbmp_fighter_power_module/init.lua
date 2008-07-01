aadedffad0f2fa9e5d7463c2d0d538fb8a55d797
theappleofPineappe <theappleofPineappe@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2008-07-01 01:26:09 +0000

    Updated the fighter module Now blows up when the shield is overloaded (explosives do the job nicely) Now the resources replenish correctly.

--- trunk/SpacebuildModelPackAlpha2/lua/entities/sbmp_fighter_power_module/init.lua
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/sbmp_fighter_power_module/init.lua
@@ -23,7 +23,7 @@ ENT.Resources     = {"energy", "air", "coolant", "ammo_basic", "ammo_pierce", "a
 ENT.ResourceNames = {"Energy", "Air", "Coolant", "Conventional Ammunition", "Piercing Ammunition", "Explosive Ammunition", "Conventional Fuel"}
 ENT.OutputTable   = {"Energy", "Air", "Coolant", "Conventional Ammunition", "Piercing Ammunition", "Explosive Ammunition", "Conventional Fuel", "Axis 1", "Axis 2", "Axis 3", "Axis 4", "Axis 5", "Axis 6", "Button 1", "Button 2", "Button 3", "Button 4", "Button 5", "Button 6", "Button 7", "Button 8", "Button 9", "Button 10"}
 ENT.MaxAmounts    = {2000, 500, 1500, 1500, 1000, 1000, 2000}
-ENT.RechargeRates = {200,  25,  100,  100,  50,   50,   100}
+ENT.RechargeRates = {25, 25, 25, 25, 25, 25, 25}
 
 ENT.TimeTo		  = 1
 ENT.MaxAng		  = 100000000
@@ -90,17 +90,20 @@ function ENT:Initialize()
 end
 
 function ENT:Think()
+	if self.Sploding then return end
 	local phys = self:GetPhysicsObject()
 	if phys and phys:IsValid() then
 		phys:Wake()
 	end
+	
 	for k, res in ipairs(self.Resources) do
 		local diff = (self.MaxAmounts[k] - RD_GetResourceAmount(self.Entity, res))
 		if diff > 0 then
-			RD_SupplyResource(self.Entity, res, math.Clamp(diff,0,self.MaxAmounts[k]))
+				RD_SupplyResource(self.Entity, res, math.Clamp(diff,0, self.RechargeRates[k]))
 		end
 	end
-	self:ProcessContraption()
+	
+	self:ProcessContraption()	
 	self.Entity:NextThink(CurTime() + 1)
 end
 
@@ -109,7 +112,7 @@ function ENT:ProcessContraption()
 	local consts = {}
 	local energy = RD_GetResourceAmount(self.Entity,"energy")
 	local coolant = RD_GetResourceAmount(self.Entity,"coolant")
-	local heating_cost = 0
+	local energy_cost = 0
 	local cooling_cost = 0
 	
 	if self.entities then
@@ -129,9 +132,9 @@ function ENT:ProcessContraption()
 
 			local cost = math.abs(ent.heat or 0)/5
 			
-			if not self.ShieldDamaged then
+			//if not self.ShieldDamaged then
 				ent.Shield = self.Entity
-			end
+			//end
 			
 			if not ent.heat then ent.heat = 0 end
 			
@@ -156,11 +159,12 @@ function ENT:ProcessContraption()
 			end
 		end
 	end
+	if energy_cost > 0 then Msg("Shielding cost for contraption (energy): ", energy_cost, "\n") end
 	
-	RD_ConsumeResource(self, "energy",  heating_cost)
-	RD_ConsumeResource(self, "coolant", cooling_cost)
+	RD_ConsumeResource(self.Entity, "energy",  energy_cost)
+	RD_ConsumeResource(self.Entity, "coolant", cooling_cost)
 	
-	for k,v in ipairs(self.Resources) do	
+	for k,v in ipairs(self.Resources) do
 		Wire_TriggerOutput(self.Entity, self.ResourceNames[k], RD_GetResourceAmount(self.Entity, v))
 	end
 end
@@ -168,7 +172,7 @@ end
 function ENT:GetNextPart(ent,EntTable,ConstraintTable)
     if (!ent:IsValid()) then return end
 	EntTable[ ent:EntIndex() ] = ent
-    if (!constraint.HasConstraints(ent)) then return end
+    if not constraint.HasConstraints(ent) then return end
     for key, ConstraintEntity in pairs(ent.Constraints) do
         if (!ConstraintTable[ ConstraintEntity ]) then
             ConstraintTable[ ConstraintEntity ] = true
@@ -187,39 +191,137 @@ function ENT:GetNextPart(ent,EntTable,ConstraintTable)
 end
 
 function ENT:ShieldDamage(amount)
+	if self.Sploding then return end
 	if self.ShieldDamaged then //fuck, terminal damage!
 		self.TerminalDamage = self.TerminalDamage + amount
 		if self.TerminalDamage >= MaxTerminalDamage then
-			self:Detonation()
+			self.Sploding = true
+			timer.Simple(1, self.Detonate, self)
+			RunConsoleCommand("stopsounds")
 		end
 	else
 		local power = RD_GetResourceAmount(self.Entity,"energy")
-		if power-amount >= 0 then
-			RD_ConsumeResource(self, "energy", amount)
+		//Msg("shield damnage\n")
+		//Msg("power: ", power, "\n")
+		//Msg("amout: ", amount, "\n")
+		
+		if (power - amount) >= 0 then
+			RD_ConsumeResource(self.Entity, "energy", amount)
 		else
-			RD_ConsumeResource(self, "energy", power)
+			RD_ConsumeResource(self.Entity, "energy", power)
 			self.ShieldDamaged = true
 			self.ShieldRepairStatus = 0
 			self:SoundAlarm()
 			timer.Simple(1,self.RestoreTimerTick,self)
 		end
+		//Msg("final energy balance: ", RD_GetResourceAmount(self.Entity,"energy"), "\n")
 	end
 end
 
 function ENT:Detonate() //"Poor Johny...." -- (Guess from where this came from....)
-	local Dir = self:GetVelocity():Normalize()
-	local Pos = self:OBBCenter()+self:GetPos()
-		
-	local fx_Disruption = EffectData()
-	local fx_Explode = EffectData()
-	local fx_Shockwave = EffectData()
+	self.entities, consts = self:GetNextPart(self.Entity, {}, {})
+	if self.entities then
+		for k,v in pairs(self.entities) do
+			v.Shield = nil
+		end
+	end
+
+	self.Entity:StopSound("coast.siren_citizen")
+	self.Entity:EmitSound("explode_9")
 	
-	fx_Disruption:SetOrigin(Pos)
-	fx_Explode:SetMagnitude(math.random(7,13))
-	fx_Explode:SetOrigin(Pos)
-	fx_Explode:SetScale(50)
-	fx_Shockwave:SetMagnitude(1)
-	fx_Shockwave:SetOrigin(Pos)
+	local Pos = self.Entity:GetPos()
+	
+	local effectdata = EffectData()
+	effectdata:SetMagnitude(1)
+	effectdata:SetOrigin(Pos)
+	effectdata:SetScale(23000)
+	util.Effect("sbmp_fighter_module_explode", effectdata)
+	
+	local tAllPlayers = player.GetAll()
+	for _, pPlayer in pairs(tAllPlayers) do
+		pPlayer.Entity:EmitSound("explode_9")
+	end
+	
+	
+	local blastradius = 3000
+	
+	for _,Ent in pairs(constraint.GetAllConstrainedEntities(self.Entity)) do
+		if Ent != self.Entity then
+			if (string.find(Ent.Entity:GetClass(),"prop") ~= nil) then
+				local delay = (math.random(300, 700) / 100)
+				Ent.Entity:SetSolid(SOLID_NONE) --we don't need to be crunching the phys collisions too
+				Ent.Entity:DrawShadow(false)
+				Ent.Entity:SetKeyValue("exploderadius","1")
+				Ent.Entity:SetKeyValue("explodedamage","1")
+				Ent.Entity:Fire("break","",tostring(delay))
+				Ent.Entity:Fire("kill","",tostring(delay + 1))
+				Ent.Entity:Fire("enablemotion","",0) --bye bye fort that took you 4 hours to make
+				constraint.RemoveAll(Ent.Entity)
+			else
+				if (Ent.Entity:IsValid()) then
+					Ent.Entity:Remove()
+				end
+			end
+		
+		end
+	end
+	
+	for key,found in pairs(ents.FindInSphere(Pos,blastradius)) do
+		movetype = 	found:GetMoveType()
+		if (movetype == 2 or movetype == 3 or movetype == 5 or movetype == 6 or movetype == 8 or movetype == 9) then
+			local entpos = found:LocalToWorld(found:OBBCenter()) --more accurate than getpos
+			local vecang = entpos - Pos
+			if vecang.z < 0 then vecang.z = 0 end
+			vecang:Normalize()
+		
+			if found:IsNPC() then --ugh, messy
+				util.BlastDamage(self.Entity, self.Entity, found:GetPos(), 256, 512)
+			elseif found:IsPlayer() then
+				found:SetModel("models/player/charple01.mdl")
+				util.BlastDamage(self.Entity, self.Entity, found:GetPos(), 256, 512)
+			elseif found:IsValid() then
+				
+				local physobj = found:GetPhysicsObject()
+				local mass = 1
+				if(physobj:IsValid()) then
+					mass = physobj:GetMass();
+				end
+				
+				if (found:GetMoveType() ~= 6) or not physobj:IsValid() then --if it's not a physics object...
+					found:SetVelocity(vecang*(170*mass)) --push it away
+				elseif (string.find(found:GetClass(),"ragdoll") ~= nil) then --if it's a ragdoll...
+					physobj:ApplyForceCenter(vecang*(8e4*mass)) --push it away anyway :D
+				else -- if it is a physics object...
+					physobj:ApplyForceOffset(vecang*(8e4*mass),entpos + Vector(math.random(-20,20),math.random(-20,20),math.random(20,40))) --still push it away
+				end
+				
+				util.BlastDamage(self.Entity, self.Entity, Pos - vecang*64, 384, 1000) --splode it
+			end
+		end
+	end
+	
+	local shake = ents.Create("env_shake")
+	shake:SetKeyValue("amplitude", "16")
+	shake:SetKeyValue("duration", "6")
+	shake:SetKeyValue("radius", 1) 
+	shake:SetKeyValue("spawnflags", 5) 
+	shake:SetKeyValue("frequency", "240")
+	shake:SetPos(Pos)
+	shake:Spawn()
+	shake:Fire("StartShake","","0.6")
+	shake:Fire("kill","","8")
+	
+	--shatter all glass
+	for k,v in pairs(ents.FindByClass("func_breakable_surf")) do
+		local dist = (v:GetPos() - Pos):Length()
+		v:Fire("Shatter","",dist/19e3)
+	end
+	
+	for k,v in pairs(ents.FindByClass("func_breakable")) do
+		local dist = (v:GetPos() - Pos):Length()
+		v:Fire("break","",dist/19e3)
+	end
+	self.Entity:Remove()
 end
 
 function ENT:SoundAlarm()
@@ -341,6 +443,13 @@ function ENT:PostEntityPaste(ply, ent, CreatedEntities)
 	end
 	table.insert(ply.SBMP_JSEnts,ent)
 end
+
+function ENT:OnTakeDamage(dmg)
+	if self.Sploding then return end
+	CDS_ShieldImpact(self:GetPos())
+	self:ShieldDamage(dmg:GetDamage())
+end
+
 
 local function SBMP_JSUpdate_CC(pl,cmd,args)
 	if not pl.SBMP_JSEnts then
