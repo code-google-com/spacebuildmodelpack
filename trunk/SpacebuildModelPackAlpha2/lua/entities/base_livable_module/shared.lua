51918a9a5712704eb0fc5050915af1fc012d73c9
stijn.sv <stijn.sv@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2008-08-14 14:44:30 +0000

    [Update] base_livable_module

--- trunk/SpacebuildModelPackAlpha2/lua/entities/base_livable_module/shared.lua
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/base_livable_module/shared.lua
@@ -2,4 +2,4 @@ ENT.Type 		= "anim"
 ENT.Base 		= "base_sb_environment"
 ENT.PrintName 	= "SBMP Ship module"
 
-list.Set( "LSEntOverlayText" , "base_livable_module", {HasOOO = true, num = -1} )
+list.Set( "LSEntOverlayText" , "base_livable_module", {HasOOO = true, resnames ={ "oxygen", "energy", "water", "nitrogen"}} )
