45b729baeaeb53461334c1a9deda4e3d027ccaaf
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-06 00:17:19 +0000

    Small Transport now upgraded. Did you know that there was a minor error in the code for the transports which meant they were using the Clunker's motion controller. The interesting thing is that it made no difference at the time.

--- SpacebuildModelPackAlpha2/lua/entities/SmallTransport/init.lua
+++ SpacebuildModelPackAlpha2/lua/entities/SmallTransport/init.lua
@@ -29,6 +29,7 @@ function ENT:Initialize()
 	self.TSpeed = 150
 	self.Active = false
 	self.Skewed = true
+	self.HSpeed = 0
 	
 	self.HPC			= 0
 	self.HP				= {}
@@ -53,7 +54,6 @@ function ENT:Initialize()
 	self.HP[5]["Type"]	= "Small"
 	self.HP[5]["Pos"]	= Vector(-50,215,40)
 	
-	
 end
 
 function ENT:SpawnFunction( ply, tr )
@@ -61,7 +61,7 @@ function ENT:SpawnFunction( ply, tr )
 	if ( !tr.Hit ) then return end
 	
 	
-	local ent = ents.Create( "Clunker" )
+	local ent = ents.Create( "SmallTransport" )
 	ent:SetPos( Vector( 100000,100000,100000 ) )
 	ent:Spawn()
 	ent:Initialize()
@@ -90,13 +90,104 @@ function ENT:SpawnFunction( ply, tr )
 	ent2.Cont = ent
 	
 	return ent
+end
+
+local STransjcon = {}	
+local STransJoystickControl = function()
+	--Joystick control stuff
+	
+	STransjcon.pitch = jcon.register{
+		uid = "strans_pitch",
+		type = "analog",
+		description = "Pitch",
+		category = "Small Transport",
+	}
+	STransjcon.yaw = jcon.register{
+		uid = "strans_yaw",
+		type = "analog",
+		description = "Yaw",
+		category = "Small Transport",
+	}
+	STransjcon.roll = jcon.register{
+		uid = "strans_roll",
+		type = "analog",
+		description = "Roll",
+		category = "Small Transport",
+	}
+	STransjcon.thrust = jcon.register{
+		uid = "strans_thrust",
+		type = "analog",
+		description = "Thrust",
+		category = "Small Transport",
+	}
+	STransjcon.accelerate = jcon.register{
+		uid = "strans_accelerate",
+		type = "analog",
+		description = "Accelerate/Decelerate",
+		category = "Small Transport",
+	}
+	STransjcon.up = jcon.register{
+		uid = "strans_strafe_up",
+		type = "digital",
+		description = "Strafe Up",
+		category = "Small Transport",
+	}
+	STransjcon.down = jcon.register{
+		uid = "strans_strafe_down",
+		type = "digital",
+		description = "Strafe Down",
+		category = "Small Transport",
+	}
+	STransjcon.right = jcon.register{
+		uid = "strans_strafe_right",
+		type = "digital",
+		description = "Strafe Right",
+		category = "Small Transport",
+	}
+	STransjcon.left = jcon.register{
+		uid = "strans_strafe_left",
+		type = "digital",
+		description = "Strafe Left",
+		category = "Small Transport",
+	}
+	STransjcon.launch = jcon.register{
+		uid = "strans_launch",
+		type = "digital",
+		description = "Launch",
+		category = "Small Transport",
+	}
+	STransjcon.switch = jcon.register{
+		uid = "strans_switch",
+		type = "digital",
+		description = "Yaw/Roll Switch",
+		category = "Small Transport",
+	}
+	STransjcon.fire1 = jcon.register{
+		uid = "strans_fire1",
+		type = "digital",
+		description = "Fire 1",
+		category = "Small Transport",
+	}
+	STransjcon.fire2 = jcon.register{
+		uid = "strans_fire2",	
+		type = "digital",
+		description = "Fire 2",
+		category = "Small Transport",
+	}
 	
 end
+
+hook.Add("JoystickInitialize","STransJoystickControl",STransJoystickControl)
 
 function ENT:Think()
 	if self.Pod and self.Pod:IsValid() then
 		self.CPL = self.Pod:GetPassenger()
 		if (self.CPL && self.CPL:IsValid()) then
+			local trace = {}
+			trace.start = self.CPL:GetShootPos()
+			trace.endpos = self.CPL:GetShootPos() + self.CPL:GetAimVector() * 10000
+			trace.filter = self.Pod
+			self.Pod.Trace = util.TraceLine( trace )
 			self.Active = true
 			if (self.CPL:KeyDown( IN_FORWARD )) then
 				if self.MCC then
@@ -133,8 +224,10 @@ function ENT:Think()
 				if !self.MTog then
 					if self.MCC then
 						self.MCC = false
+						self.CPL:PrintMessage( HUD_PRINTCENTER, "Mouse Control Disabled" )
 					else
-						--self.MCC = true
+						self.MCC = true
+						self.CPL:PrintMessage( HUD_PRINTCENTER, "Mouse Control Enabled" )
 					end
 				end
 				self.MTog = true
@@ -142,7 +235,7 @@ function ENT:Think()
 				self.MTog = false
 			end
 			
-			if (self.CPL:KeyDown( IN_JUMP )) then
+			if (self.CPL:KeyDown( IN_JUMP ) || (joystick && joystick.Get(self.CPL, "strans_launch"))) then
 				if !self.LTog then
 					if self.Launchy then
 						self.Launchy = false
@@ -158,49 +251,135 @@ function ENT:Think()
 				self.LTog = false
 			end
 			
-			if (self.CPL:KeyDown( IN_ATTACK )) then
-				for i = 1, self.HPC do
-					local HPC = self.CPL:GetInfo( "SBHP_"..i )
-					if self.HP[i]["Ent"] && self.HP[i]["Ent"]:IsValid() && (HPC == "1.00" || HPC == "1" || HPC == 1) then
-						self.HP[i]["Ent"].Entity:HPFire()
+			if (joystick) then
+				if (joystick.Get(self.CPL, "strans_strafe_up")) then
+					self.VSpeed = 50
+				elseif (joystick.Get(self.CPL, "strans_strafe_down")) then
+					self.VSpeed = -50
+				end
+			
+				if (joystick.Get(self.CPL, "strans_strafe_right")) then
+					self.HSpeed = 50
+				elseif (joystick.Get(self.CPL, "strans_strafe_left")) then
+					self.HSpeed = -50
+				else
+					self.HSpeed = 0
+				end
+			
+				--Acceleration, greater than halfway accelerates, less than decelerates
+				if (joystick.Get(self.CPL, "strans_accelerate")) then
+					if (joystick.Get(self.CPL, "strans_accelerate") > 128) then
+						self.Speed = math.Clamp(self.Speed + (joystick.Get(self.CPL, "strans_accelerate")/127.5-1)*5, -40, 2000)
+					elseif (joystick.Get(self.CPL, "strans_accelerate") < 127) then
+						self.Speed = math.Clamp(self.Speed + (joystick.Get(self.CPL, "strans_accelerate")/127.5-1)*10, -40, 2000)
+					end
+				end
+				
+				--Set the speed
+				if (joystick.Get(self.CPL, "strans_thrust")) then
+					if (joystick.Get(self.CPL, "strans_thrust") > 128) then
+						self.TarSpeed = (joystick.Get(self.CPL, "strans_thrust")/127.5-1)*2000
+					elseif (joystick.Get(self.CPL, "strans_thrust") < 127) then
+						self.TarSpeed = (joystick.Get(self.CPL, "strans_thrust")/127.5-1)*40
+					elseif (joystick.Get(self.CPL, "strans_thrust") < 128 && joystick.Get(self.CPL, "strans_thrust") > 127) then
+						self.TarSpeed = 0
+					end
+					if (self.TarSpeed > self.Speed) then
+						self.Speed = math.Clamp(self.Speed + 5, -40, 2000)
+					elseif (self.TarSpeed < self.Speed) then
+						self.Speed = math.Clamp(self.Speed - 10, -40, 2000)						
+					end
+				end
+				
+				--forward is down on pitch, if you don't like it check the box on joyconfig to inver it
+				if (joystick.Get(self.CPL, "strans_pitch")) then
+					if (joystick.Get(self.CPL, "strans_pitch") > 128) then
+						self.Pitch = -(joystick.Get(self.CPL, "strans_pitch")/127.5-1)*90
+					elseif (joystick.Get(self.CPL, "strans_pitch") < 127) then
+						self.Pitch = -(joystick.Get(self.CPL, "strans_pitch")/127.5-1)*90
+					elseif (joystick.Get(self.CPL, "strans_pitch") < 128 && joystick.Get(self.CPL, "strans_pitch") > 127) then
+						self.Pitch = 0
+					end
+				end
+			
+				--The control for inverting yaw and roll
+				local yaw = ""
+				local roll = ""
+				if (joystick.Get(self.CPL, "strans_switch")) then
+					yaw = "strans_roll"
+					roll = "strans_yaw"
+				else
+					yaw = "strans_yaw"
+					roll = "strans_roll"
+				end
+				
+				--Yaw is negative because Paradukes says so
+				--You could invert it, but the default configuration should be correct
+				if (joystick.Get(self.CPL, yaw)) then
+					if (joystick.Get(self.CPL, yaw) > 128) then
+						self.Yaw = -(joystick.Get(self.CPL, yaw)/127.5-1)*90
+					elseif (joystick.Get(self.CPL, yaw) < 127) then
+						self.Yaw = -(joystick.Get(self.CPL, yaw)/127.5-1)*90
+					elseif (joystick.Get(self.CPL, yaw) < 128 && joystick.Get(self.CPL, yaw) > 127) then
+						self.Yaw = 0
+					end
+				end
+			
+				if (joystick.Get(self.CPL, roll)) then
+					if (joystick.Get(self.CPL, roll) > 128) then
+						self.Roll = (joystick.Get(self.CPL, roll)/127.5-1)*90
+					elseif (joystick.Get(self.CPL, roll) < 127) then
+						self.Roll = (joystick.Get(self.CPL, roll)/127.5-1)*90
+					elseif (joystick.Get(self.CPL, roll) < 128 && joystick.Get(self.CPL, roll) > 127) then
+						self.Roll = 0
 					end
 				end
 			end
 			
-			if (self.CPL:KeyDown( IN_ATTACK2 )) then
+			if (self.CPL:KeyDown( IN_ATTACK ) || (joystick && joystick.Get(self.CPL, "strans_fire1"))) then
+				for i = 1, self.HPC do
+					local HPC = self.CPL:GetInfo( "SBHP_"..i )
+					if self.HP[i]["Ent"] && self.HP[i]["Ent"]:IsValid() && (HPC == "1.00" || HPC == "1" || HPC == 1) then
+						if self.HP[i]["Ent"].Cont && self.HP[i]["Ent"].Cont:IsValid() then
+							self.HP[i]["Ent"].Cont:HPFire()
+						else
+							self.HP[i]["Ent"].Entity:HPFire()
+						end
+					end
+				end
+			end
+			
+			if (self.CPL:KeyDown( IN_ATTACK2 ) || (joystick && joystick.Get(self.CPL, "strans_fire2"))) then
 				for i = 1, self.HPC do
 					local HPC = self.CPL:GetInfo( "SBHP_"..i.."a" )
 					if self.HP[i]["Ent"] && self.HP[i]["Ent"]:IsValid() && (HPC == "1.00" || HPC == "1" || HPC == 1) then
-						self.HP[i]["Ent"].Entity:HPFire()
+						if self.HP[i]["Ent"].Cont && self.HP[i]["Ent"].Cont:IsValid() then
+							self.HP[i]["Ent"].Cont:HPFire()
+						else
+							self.HP[i]["Ent"].Entity:HPFire()
+						end
 					end
 				end
 			end
 			
 			if self.MCC then
-				local PAng = self.CPL:EyeAngles()
-				local VAng = self.Pod:GetAngles()
-				local AAng = Angle(0,0,0)
+				local PRel = self.Pod:GetPos() + self.CPL:GetAimVector() * 100
 				
-				--self.CPL:PrintMessage( HUD_PRINTCENTER, "Player: " .. math.Round(PAng.y) .. ", " .. math.Round(PAng.r) )
-				--self.CPL:PrintMessage( HUD_PRINTCENTER, "Player: " .. math.Round(VAng.y) .. ", " .. math.Round(VAng.r) )
-				
-				AAng.r = VAng.r
-				AAng.p = PAng.p - VAng.p
-				AAng.z = PAng.y - VAng.y
-				
-				--self.CPL:PrintMessage( HUD_PRINTCENTER, "Player: " .. math.Round(AAng.y) .. ", " .. math.Round(AAng.r) ) 
-				
-				CYaw = (AAng.y * math.cos(math.rad(AAng.r))) - (AAng.p * math.sin(math.rad(AAng.r)))
-				CPitch = (AAng.y * math.sin(math.rad(AAng.r))) - (AAng.p * math.cos(math.rad(AAng.r)))
-				
-				self.CPL:PrintMessage( HUD_PRINTCENTER, "Player: " .. math.Round(CYaw) .. ", " .. math.Round(CPitch) ) 
+				--Believe it or not, the following code came from a set of tank treads. Who'd have thunk it?
+				local FDist = PRel:Distance( self.Pod:GetPos() + self.Pod:GetUp() * 500 )
+				local BDist = PRel:Distance( self.Pod:GetPos() + self.Pod:GetUp() * -500 )
+				self.Pitch = (FDist - BDist) * 0.5
+				FDist = PRel:Distance( self.Pod:GetPos() + self.Pod:GetForward() * 500 )
+				BDist = PRel:Distance( self.Pod:GetPos() + self.Pod:GetForward() * -500 )
+				self.Yaw = (BDist - FDist) * 0.5
+
 				self.CPL:CrosshairEnable()
 			end
 			
 			if (self.Launchy) then
 				local physi = self.Pod:GetPhysicsObject()
-				physi:SetVelocity( (physi:GetVelocity() * 0.75) + ((self.Pod:GetRight() * self.Speed) + (self.Pod:GetUp() * self.VSpeed)) )
-				physi:AddAngleVelocity((physi:GetAngleVelocity() * -1) + Angle(self.Roll,self.Pitch,self.Yaw))
+				physi:SetVelocity( (physi:GetVelocity() * 0.75) + ((self.Pod:GetRight() * self.Speed) + (self.Pod:GetUp() * self.VSpeed) + (self.Pod:GetForward() * -self.HSpeed)) )
+				physi:AddAngleVelocity((physi:GetAngleVelocity() * -0.75) + Angle(self.Roll,self.Pitch,self.Yaw))
 				physi:EnableGravity(false)
 			else
 				self.Speed = 0
@@ -215,6 +394,7 @@ function ENT:Think()
 			self.Yaw = 0
 			self.Roll = 0
 			self.Pitch = 0
+			self.Pod.Trace = nil
 		end
 	else
 		self.Entity:Remove()
@@ -239,7 +419,7 @@ function ENT:Touch( ent )
 end
 
 function ENT:OnRemove()
-	if self.Pod:IsValid() then
+	if self.Pod && self.Pod:IsValid() then
 		self.Pod:Remove()
 	end
 end
