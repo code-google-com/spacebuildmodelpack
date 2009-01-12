543610cda350f5a7677430850b69e8902417cd32
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-12 18:49:24 +0000

    Hangars now dupe even with fighters in, go on, make some huge carriers.

--- trunk/SpacebuildModelPackAlpha2/lua/entities/SWORDHangarLarge/shared.lua
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/SWORDHangarLarge/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 			= "anim"
-ENT.Base 			= "base_gmodentity"
+ENT.Base 			= "base_wire_entity"
 ENT.PrintName		= "Large SWORD Hangar"
 ENT.Author			= "fishface60"
 ENT.Category		= "SBEP - Other"
