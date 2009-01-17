cdfe838eec43b2e5710c18fba82d0b33b0e89254
unintendedhysteria <unintendedhysteria@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-17 16:00:24 +0000

    Added the HEVSuit player models.

---
+++ SpacebuildModelPackAlpha2/lua/autorun/sbep_player_models.lua
@@ -0,0 +1,55 @@
+if (SERVER) then
+	player_manager.AddValidModel( "SBEPRedHEV", "models/SBEP Player Models/redhevsuit.mdl" ) 
+	player_manager.AddValidModel( "SBEPBlueHEV", "models/SBEP Player Models/bluehevsuit.mdl" )
+	player_manager.AddValidModel( "SBEPOrangeHEV", "models/SBEP Player Models/orangehevsuit.mdl" )
+	AddCSLuaFile( "sbep_player_models.lua" )
+end
+
+list.Set( "PlayerOptionsModel",  "SBEPRedHEV", "models/SBEP Player Models/redhevsuit.mdl" )
+list.Set( "PlayerOptionsModel",  "SBEPBlueHEV", "models/SBEP Player Models/bluehevsuit.mdl" )
+list.Set( "PlayerOptionsModel",  "SBEPOrangeHEV", "models/SBEP Player Models/orangehevsuit.mdl" )
+
+local NPC =
+{
+	Name = "Red HEVSuit Guy",
+	Class = "npc_citizen",
+	KeyValues =
+	{
+		citizentype = 4
+	},
+	Model = "models/SBEP Player Models/redhevsuit.mdl",
+	Health = "150",
+	Category = "Humans + Resistance"
+}
+
+list.Set( "NPC", "npc_redhevsuit", NPC )
+
+local NPC =
+{
+	Name = "Blue HEVSuit Guy",
+	Class = "npc_citizen",
+	KeyValues =
+	{
+		citizentype = 4
+	},
+	Model = "models/SBEP Player Models/bluehevsuit.mdl",
+	Health = "150",
+	Category = "Humans + Resistance"
+}
+
+list.Set( "NPC", "npc_bluehevsuit", NPC )
+
+local NPC =
+{
+	Name = "Orange HEVSuit Guy",
+	Class = "npc_citizen",
+	KeyValues =
+	{
+		citizentype = 4
+	},
+	Model = "models/SBEP Player Models/orangehevsuit.mdl",
+	Health = "150",
+	Category = "Humans + Resistance"
+}
+
+list.Set( "NPC", "npc_orangehevsuit", NPC )
