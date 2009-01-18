a66a6897de1fcf78e3dfa95897d65ba5ce43a04c
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-18 15:48:56 +0000

    Smallbridge hangars added. Medbridge minelayer should now launch homing mines.

---
+++ SpacebuildModelPackAlpha2/lua/entities/sbmp_hangar_sb/init.lua
@@ -0,0 +1,70 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+function ENT:Initialize()
+
+	self.Entity:SetModel( "models/SmallBridge/SBdropbaycomplete1S/sbdropbaycomplete1s.mdl" )
+	self.Entity:SetName("sbHangar")
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+	local phys = self.Entity:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+		phys:EnableGravity(true)
+		phys:EnableDrag(true)
+		phys:EnableCollisions(true)
+	end
+	
+    self.Entity:SetKeyValue("rendercolor", "255 255 255")
+	self.Bay = {}
+	self.Bay[1] = {}
+	self.Bay[1]["ship"] = nil
+	self.Bay[1]["weld"] = nil
+	self.Bay[1]["pos"] = Vector(0,0,0)
+	self.Bay[1]["canface"] = {Angle(0,0,0),Angle(0,180,0),Angle(90,90,0),Angle(90,180,0),Angle(90,270,0),Angle(90,360,0)}
+	self.Bay[1]["pexit"] = Vector(-250,0,10)
+	
+	self.LaunchSpeed = 1
+	
+end
+
+function ENT:SpawnFunction( ply, tr )
+
+	if ( !tr.Hit ) then return end
+	
+	local SpawnPos = tr.HitPos + tr.HitNormal * 16 + Vector(0,0,500)
+	
+	local ent = ents.Create( "sbmp_hangar_sb" )
+	ent:SetPos( SpawnPos )
+	ent:Spawn()
+	ent:Initialize()
+	ent:Activate()
+	ent.SPL = ply
+	
+	return ent
+	
+end
+
+function ENT:BuildDupeInfo()
+	local info = self.BaseClass.BuildDupeInfo(self) or {}
+	info["ships"] = {}
+	for k, v in pairs(self.Bay) do
+		if (v.ship) and (v.ship:IsValid()) then
+			info["ships"][k] = v.ship:EntIndex()
+		end
+	end
+	return info
+end
+
+function ENT:ApplyDupeInfo(ply, ent, info, GetEntByID)
+	self.BaseClass.ApplyDupeInfo(self, ply, ent, info, GetEntByID)
+	for k, v in pairs(info.ships) do
+		self.Bay[k]["ship"] = GetEntByID(v)
+		if (!self.Bay[k]["ship"]) then
+			self.Bay[k]["ship"] = ents.GetByIndex(v)
+		end
+	end
+end
---
+++ SpacebuildModelPackAlpha2/lua/entities/sbmp_hangar_sb_clamp/init.lua
@@ -0,0 +1,70 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+function ENT:Initialize()
+
+	self.Entity:SetModel( "models/SmallBridge/SBdock1/SBdock1.mdl" )
+	self.Entity:SetName("sbclamp")
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+	local phys = self.Entity:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+		phys:EnableGravity(true)
+		phys:EnableDrag(true)
+		phys:EnableCollisions(true)
+	end
+	
+    self.Entity:SetKeyValue("rendercolor", "255 255 255")
+	self.Bay = {}
+	self.Bay[1] = {}
+	self.Bay[1]["ship"] = nil
+	self.Bay[1]["weld"] = nil
+	self.Bay[1]["pos"] = Vector(-128,0,-64)
+	self.Bay[1]["canface"] = {Angle(0,90,0),Angle(0,270,0)}
+	self.Bay[1]["pexit"] = Vector(0,0,0)
+	
+	self.LaunchSpeed = 100
+	
+end
+
+function ENT:SpawnFunction( ply, tr )
+
+	if ( !tr.Hit ) then return end
+	
+	local SpawnPos = tr.HitPos + tr.HitNormal * 16 + Vector(0,0,500)
+	
+	local ent = ents.Create( "sbmp_hangar_sb_clamp" )
+	ent:SetPos( SpawnPos )
+	ent:Spawn()
+	ent:Initialize()
+	ent:Activate()
+	ent.SPL = ply
+	
+	return ent
+	
+end
+
+function ENT:BuildDupeInfo()
+	local info = self.BaseClass.BuildDupeInfo(self) or {}
+	info["ships"] = {}
+	for k, v in pairs(self.Bay) do
+		if (v.ship) and (v.ship:IsValid()) then
+			info["ships"][k] = v.ship:EntIndex()
+		end
+	end
+	return info
+end
+
+function ENT:ApplyDupeInfo(ply, ent, info, GetEntByID)
+	self.BaseClass.ApplyDupeInfo(self, ply, ent, info, GetEntByID)
+	for k, v in pairs(info.ships) do
+		self.Bay[k]["ship"] = GetEntByID(v)
+		if (!self.Bay[k]["ship"]) then
+			self.Bay[k]["ship"] = ents.GetByIndex(v)
+		end
+	end
+end
