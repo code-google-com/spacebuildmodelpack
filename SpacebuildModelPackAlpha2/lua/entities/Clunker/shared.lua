fc175652c0f611f9cdb402f872497797a568f295
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-21 18:02:22 +0000

    Fighters and Transports now run off base classes, this will make it easier to upgrade movement code and easier to make new ships.

--- SpacebuildModelPackAlpha2/lua/entities/Clunker/shared.lua
+++ SpacebuildModelPackAlpha2/lua/entities/Clunker/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 			= "anim"
-ENT.Base 			= "base_wire_entity"
+ENT.Base 			= "base_transport"
 ENT.PrintName		= "Clunker"
 ENT.Author			= "Paradukes + SlyFo + fishface60"
 ENT.Category		= "SBEP-Fighters"
@@ -8,16 +8,3 @@ ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 ENT.Owner			= nil
 ENT.CPL				= nil
-
-ENT.EPrs			= false
-ENT.TSpeed			= 75
-ENT.LTog			= false
-ENT.MTog			= false
-ENT.MCC				= false
-ENT.MChange			= false
-ENT.Speed			= 0
-ENT.Active			= false
-ENT.Launchy			= false
-ENT.Pitch			= 0
-ENT.Yaw				= 0
-ENT.Roll			= 0
--- SpacebuildModelPackAlpha2/lua/entities/arwing/shared.lua
+++ SpacebuildModelPackAlpha2/lua/entities/arwing/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 			= "anim"
-ENT.Base 			= "base_wire_entity"
+ENT.Base 			= "base_fighter"
 ENT.PrintName		= "Arwing"
 ENT.Author			= "Paradukes + Slyfo + Fishface60"
 ENT.Category		= "SBEP-Fighters"
@@ -8,33 +8,3 @@ ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 ENT.Owner			= nil
 ENT.CPL				= nil
-
-/*
-ENT.HPC				= 3
-ENT.HP				= {}
-ENT.HP[1]			= {}
-ENT.HP[1]["Ent"]	= nil
-ENT.HP[1]["Type"]	= "Large"
-ENT.HP[1]["Pos"]	= Vector(-140,40,35)
-ENT.HP[2]			= {}
-ENT.HP[2]["Ent"]	= nil
-ENT.HP[2]["Type"]	= "Large"
-ENT.HP[2]["Pos"]	= Vector(140,40,35)
-ENT.HP[3]			= {}
-ENT.HP[3]["Ent"]	= nil
-ENT.HP[3]["Type"]	= "Medium"
-ENT.HP[3]["Pos"]	= Vector(0,-75,30)
-*/
-
-ENT.EPrs			= false
-ENT.TSpeed			= 75
-ENT.LTog			= false
-ENT.MTog			= false
-ENT.MCC				= false
-ENT.MChange			= false
-ENT.Speed			= 0
-ENT.Active			= false
-ENT.Launchy			= false
-ENT.Pitch			= 0
-ENT.Yaw				= 0
-ENT.Roll			= 0
