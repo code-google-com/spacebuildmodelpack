fe48ef2dae5d473561eeede3d66bbf9e62e5aa3b
unintendedhysteria <unintendedhysteria@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-22 22:05:13 +0000

    Switched the doors to use a base door entity, added the Iris door, and deleted a couple of outdated spawnicons.

---
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/sbep_base_door/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type			      = "anim"
+ENT.Base                  = "base_wire_entity" 
+ENT.PrintName		      = "Blast Door"
+ENT.Author			      = "Hysteria"
+ENT.Category			  = "SBEP - Other"
+
+ENT.AutomaticFrameAdvance = true
+ENT.Spawnable			  = false
+ENT.AdminSpawnable		  = false
+
+ENT.Purpose 		      = "To open and close."
