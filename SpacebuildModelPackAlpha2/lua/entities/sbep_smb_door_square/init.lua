407dd6f1abadc1667341c57cf0cedcefffce97bb
unintendedhysteria <unintendedhysteria@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-01-22 22:05:13 +0000

    Switched the doors to use a base door entity, added the Iris door, and deleted a couple of outdated spawnicons.

---
+++ SpacebuildModelPackAlpha2/lua/entities/sbep_smb_door_square/init.lua
@@ -0,0 +1,52 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( "shared.lua" ) 
+
+local model = { "models/SmallBridge/SBdooranim2/SBdooranim2.mdl" , "models/SmallBridge/SBdoorsquare/SBdoorsquare.mdl" }
+
+function ENT:Initialize()	
+
+		self:SetModel( model[1] )
+		self:PhysicsInit( SOLID_VPHYSICS )
+		self:SetMoveType( MOVETYPE_VPHYSICS )
+		self:SetSolid( SOLID_VPHYSICS )
+		
+		local phys = self:GetPhysicsObject()  	
+		if (phys:IsValid()) then  		
+			phys:Wake()  	
+		end 
+		
+		self:ResetSequence( self:LookupSequence( "close" ) )
+		self:SetPlaybackRate( 1 )
+
+		self.Opened       = false
+		self.Delay      = true
+		self.Locked     = false
+		self.DisableUse = false
+		self.DoorModel = model[2]
+
+		if !self.SBdoor or !self.SBdoor:IsValid() then
+			self.SBdoor = ents.Create( "prop_physics" )
+		end
+		self:AddDoorPhysics()
+		
+		self.Inputs = Wire_CreateInputs(self.Entity, { "Open" , "Lock" , "Disable Use" })
+
+end
+
+function ENT:SpawnFunction( ply, tr )
+
+	if ( !tr.Hit ) then return end
+	
+	local SpawnPos = tr.HitPos + tr.HitNormal * 16 + Vector(0,0,65.1)
+	local RotAng   = Angle(180 , 0 , 0)
+	
+	local ent = ents.Create( "sbep_smb_door_square" )
+	ent:SetPos( SpawnPos )
+	ent:SetAngles( RotAng )
+	ent:Spawn()
+	ent:Activate()
+	
+	return ent
+	
+end
