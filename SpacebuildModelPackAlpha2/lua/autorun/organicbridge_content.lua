dbd22881ffe0a10baab2ec5bdb8ffd6b81bc7ac9
robojoos <robojoos@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-02-07 06:46:05 +0000

    squissquishies! <- what pjb calls organicbridge.

---
+++ SpacebuildModelPackAlpha2/lua/autorun/organicbridge_content.lua
@@ -0,0 +1,6 @@
+--[Organic Bridge - Content by Cerebrate
+--[This file takes care of all organicbridge content
+--[Created by Cerebrate.
+
+--[Thrusters
+list.Set( "ThrusterModels", "models/organicbridge/1x1engine_squid.mdl", {} )
