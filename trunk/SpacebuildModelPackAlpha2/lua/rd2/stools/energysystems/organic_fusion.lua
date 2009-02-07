944eb3915ec7f6ad048071729f8ff9d907582be0
robojoos <robojoos@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-02-07 06:46:05 +0000

    squissquishies! <- what pjb calls organicbridge.

---
+++ trunk/SpacebuildModelPackAlpha2/lua/rd2/stools/energysystems/organic_fusion.lua
@@ -0,0 +1,16 @@
+
+DEVICEGROUP.type			= "generator_energy_fusion"
+
+--[[
+	You can also use skin = number here to define a skin to make the Module spawn with
+	You can also use material = "path/to/material" to set a material to make it spawn with
+]]
+
+DEVICEGROUP.devices = {
+	cere_1 = {
+		Name		= "Organic Bridge Fusion Generator",
+		model		= "models/organicbridge/heart.mdl",
+		skin 		= 0
+	},
+}
+
