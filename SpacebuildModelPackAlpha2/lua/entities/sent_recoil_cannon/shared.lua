1a3070ff4192115990478d3f0bb28c54272f5e10
levybreak098 <levybreak098@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-01 13:17:18 +0000

    [Added] Smallbridge Biodomes, and recoil animation test entity (no, it's not a functioning weapon. (Or even a completed model) Yet.)

---
+++ SpacebuildModelPackAlpha2/lua/entities/sent_recoil_cannon/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.PrintName		= "recoil-animation cannon"
+ENT.Author			= "Levybreak"
+ENT.Contact 		= "Facepunch"
+ENT.Purpose			= "Roll. Collect. Live. Love."
+ENT.Instructions	= "Roll. Collect. Live. Love." 
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= true 
+ENT.AutomaticFrameAdvance = true
