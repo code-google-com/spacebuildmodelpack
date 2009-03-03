a6f3e65ff45d5f5726096431a185eb9849f16135
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-03 19:55:06 +0000

    UNTESTED function to replace the model paths in dupes. Uploaded mostly so the table's up there ready for it.

---
+++ trunk/SpacebuildModelPackAlpha2/lua/autorun/client/sbep_dupe_fix.lua
@@ -0,0 +1,90 @@
+SBEP = SBEP or {}
+local repTab = {}
+--Smallbridge SW HULLS/CORRIDORS--
+repTab["models/SmallBridge/SBcorridorCurveS/SBcorridorCurveS.mdl"] = "models/SmallBridge/Hulls,SW/SBhullCurveS.mdl"
+repTab["models/SmallBridge/SBcorridorE1/SBcorridorE1.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE1.mdl"
+repTab["models/SmallBridge/SBcorridorE1/SBcorridorE2.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE2.mdl"
+repTab["models/SmallBridge/SBcorridorE1/SBcorridorE3.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE3.mdl"
+repTab["models/SmallBridge/SBcorridorE1/SBcorridorE4.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE4.mdl"
+repTab["models/SmallBridge/SBcorridorE1/SBcorridorE05.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE05.mdl"
+repTab["models/SmallBridge/SBcorridorEdh/SBcorridorEdh.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh.mdl"
+repTab["models/SmallBridge/SBcorridorEdh/SBcorridorEdh2.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh2.mdl"
+repTab["models/SmallBridge/SBcorridorEdh/SBcorridorEdh3.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh3.mdl"
+repTab["models/SmallBridge/SBcorridorEdh/SBcorridorEdh4.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh4.mdl"
+repTab["models/SmallBridge/SBcorridorEFlip/SBcorridorEFlip.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEFlip.mdl"
+repTab["models/SmallBridge/SBcorridorEnd/SBcorridorEnd.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEnd.mdl"
+repTab["models/SmallBridge/SBcorridorR/SBcorridorR.mdl"] = "models/SmallBridge/Hulls,SW/SBhullR.mdl"
+repTab["models/SmallBridge/SBcorridorRtri/SBcorridorRtri.mdl"] = "models/SmallBridge/Hulls,SW/SBhullRtri.mdl"
+repTab["models/SmallBridge/SBcorridorSlanthalfL/SBcorridorSlanthalfL.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlanthalfR.mdl"
+repTab["models/SmallBridge/SBcorridorSlanthalfR/SBcorridorSlanthalfR.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlanthalfL.mdl"
+repTab["models/SmallBridge/SBcorridorSlantL/SBcorridorSlantL.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlantR.mdl"
+repTab["models/SmallBridge/SBcorridorSlantR/SBcorridorSlantR.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlantL.mdl"
+repTab["models/SmallBridge/SBcorridorT/SBcorridorT.mdl"] = "models/SmallBridge/Hulls,SW/SBhullT.mdl"
+repTab["models/SmallBridge/SBcorridorTdl/SBcorridorTdl.mdl"] = "models/SmallBridge/Hulls,SW/SBhullTdl.mdl"
+repTab["models/SmallBridge/SBcorridorTdldw/SBcorridorTdldw.mdl"] = "models/SmallBridge/Hulls,SW/SBhullTdldw.mdl"
+repTab["models/SmallBridge/SBtriangleE1/SBtriangleE1.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri1.mdl"
+repTab["models/SmallBridge/SBtriangleE2/SBtriangleE2.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri2.mdl"
+repTab["models/SmallBridge/SBtriangleE3/SBtriangleE3.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri3.mdl"
+repTab["models/SmallBridge/SBcorridorX/SBcorridorX.mdl"] = "models/SmallBridge/Hulls,SW/SBhullX.mdl"
+repTab["models/SmallBridge/SBcorridorXdl/SBcorridorXdldw.mdl"] = "models/SmallBridge/Hulls,SW/SBhullXdl.mdl"
+repTab["models/SmallBridge/SBcorridorXdldw/SBcorridorXdldw.mdl"] = "models/SmallBridge/Hulls,SW/SBhullXdldw.mdl"
+--Smallbridge Walkways--
+repTab["models/SmallBridge/SBwalkwayE/SBwalkwayE.mdl"] = "models/SmallBridge/Walkways/SBwalkwayE.mdl"
+repTab["models/SmallBridge/SBwalkwayE2/SBwalkwayE2.mdl"] = "models/SmallBridge/Walkways/SBwalkwayE2.mdl"
+repTab["models/SmallBridge/SBwalkwayR/SBwalkwayR.mdl"] = "models/SmallBridge/Walkways/SBwalkwayR.mdl"
+repTab["models/SmallBridge/SBwalkwayT/SBwalkwayT.mdl"] = "models/SmallBridge/Walkways/SBwalkwayT.mdl"
+repTab["models/SmallBridge/SBwalkwayX/SBwalkwayX.mdl"] = "models/SmallBridge/Walkways/SBwalkwayX.mdl"
+--Smallbridge Vehicles--
+repTab["models/SmallBridge/SBchair/SBchair.mdl"] = "models/SmallBridge/Vehicles/SBVPchair.mdl"
+repTab["models/SmallBridge/SBdroppod1/SBdroppod1.mdl"] = "models/SmallBridge/Vehicles/SBVdroppod1.mdl"
+--Smallbridge Other Parts--
+repTab["models/SmallBridge/SBconsole1s/SBconsole1s.mdl"] = "models/SmallBridge/Other/SBconsole.mdl"
+repTab["models/SmallBridge/SBconsole1low/SBconsole1low.mdl"] = "models/SmallBridge/Other/SBconsolelow.mdl"
+repTab["models/SmallBridge/SBconsoletop1/SBconsoletop1.mdl"] = "models/SmallBridge/Other/SBconsoletop.mdl"
+--Smallbridge SENTs/Animated--
+repTab["models/SmallBridge/SBdooranim1/SBdooranim1.mdl"] = "models/SmallBridge/SEnts/SBADoor1.mdl"
+repTab["models/SmallBridge/SBdooranim2/SBdooranim2.mdl"] = "models/SmallBridge/SEnts/SBADoor2.mdl"
+repTab["models/SmallBridge/SBdooriris2.mdl"] = "models/SmallBridge/SEnts/SBADoorIris.mdl"
+repTab["models/SmallBridge/SBdooranimwide.mdl"] = "models/SmallBridge/SEnts/SBADoorWide.mdl"
+--Smallbridge Panels/Doors--
+repTab["models/SmallBridge/SBdoor/SBdoor.mdl"] = "models/SmallBridge/Panels/SBdoor.mdl"
+repTab["models/SmallBridge/SBdoorsquare/SBdoorsquare.mdl"] = "models/SmallBridge/Panels/SBdoorSquare.mdl"
+repTab["models/SmallBridge/SBdooriris.mdl"] = "models/SmallBridge/Panels/SBdooriris.mdl"
+repTab["models/SmallBridge/SBdoorwide/SBdoorwide.mdl"] = "models/SmallBridge/Panels/SBdoorWide.mdl"
+repTab["models/SmallBridge/SBpanelSolidg/SBpanelSolidg.mdl"] = "models/SmallBridge/Panels/SBpanelSolid.mdl"
+repTab["models/SmallBridge/SBpanelSolidgdw/SBpanelSolidgdw.mdl"] = "models/SmallBridge/Panels/SBpanelSoliddw.mdl"
+repTab["models/SmallBridge/SBpanelDH/SBpanelDH.mdl"] = "models/SmallBridge/Panels/SBpanelDH.mdl"
+repTab["models/SmallBridge/SBpanelDHDW/SBpanelDHDW.mdl"] = "models/SmallBridge/Panels/SBpanelDHDW.mdl"
+repTab["models/SmallBridge/SBpanelDoor/SBpanelDoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoor.mdl"
+repTab["models/SmallBridge/SBpanelDoordw/SBpanelDoordw.mdl"] = "models/SmallBridge/Panels/SBpanelDoordw.mdl"
+repTab["models/SmallBridge/SBpanelDoor2dw/SBpanelDoor2dw.mdl"] = "models/SmallBridge/Panels/SBpanelDoordw2.mdl"
+repTab["models/SmallBridge/SBpanelSquareDoor/SBpanelSquareDoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoorSquare.mdl"
+repTab["models/SmallBridge/SBpanelSquareDoordw/SBpanelSquareDoordw.mdl"] = "models/SmallBridge/Panels/SBpanelDoorSquaredw.mdl"
+repTab["models/SmallBridge/sbdooriris2.mdl"] = "models/SmallBridge/Panels/SBpanelDoorIris.mdl"
+repTab["models/SmallBridge/SBpanelwideDoor/SBpanelwideDoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoorWide.mdl"
+repTab["models/SmallBridge/SBpanelSquareDoordockin/SBpanelSquareDoordockin.mdl"] = "models/SmallBridge/Panels/SBpanelDockIn.mdl"
+repTab["models/SmallBridge/SBpanelSquareDoordocko/SBpanelSquareDoordocko.mdl"] = "models/SmallBridge/Panels/SBpanelDockOut.mdl"
+SBEP_ReplaceTable = repTab
+SBEP.ReplaceTable = repTab
+
+function SBEP_FixDupe(player,command,args)
+	local filePath = args[1]
+	local fileString = file.Read(filePath)
+	for old,new in pairs(repTab) do
+		string.Replace(fileString,old,new)
+	end
+	file.Write(filePath,fileString)
+end
+SBEP.FixDupe = SBEP_FixDupe
+
+function SBEP_FixAllDupes(player,command,args)
+	for _,dirPath in pairs(file.FindDir("adv_duplicator/*") ) do
+		for _,filePath in pairs(file.Find(dirPath.."/*")) do
+			SBEP.FixDupe(player,command,filePath)
+		end
+	end
+end
+SBEP.FixAllDupes = SBEP_FixAllDupes
+
+concommand.Add("SBEP_FixDupe",SBEP.FixDupe)
+concommand.Add("SBEP_FixAllDupes",SBEP.FixAllDupes)
