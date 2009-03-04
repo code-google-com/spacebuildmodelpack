74dc73ca915779d5b3164b6d00a12b8d0e756542
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-04 16:12:13 +0000

    Ok, 2 new console commands added. SBEP_LoadReplaceTable: Automatically called if the table hasn't yet been loaded, it's here in case you want to change the table and try it out without restarting Garrysmod. The table isn't automatically loaded on start as you won't need it if all your dupes already work SBEP_FixDupeFolder: This looks in its folder and all subfolders for .txt files and replaces all the models from the replacement table. SBEP_FixDupeFolder adv_duplicator would work the same as SBEP_FixAllDupes.

--- SpacebuildModelPackAlpha2/lua/autorun/client/sbep_dupe_fix.lua
+++ SpacebuildModelPackAlpha2/lua/autorun/client/sbep_dupe_fix.lua
@@ -1,89 +1,69 @@
 SBEP = SBEP or {}
-local repTab = {}
---Smallbridge SW HULLS/CORRIDORS--
-repTab["models/SmallBridge/SBcorridorCurveS/sbcorridorcurves.mdl"] = "models/SmallBridge/Hulls,SW/SBhullCurveS.mdl"
-repTab["models/SmallBridge/SBcorridorE1/sbcorridore1.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE1.mdl"
-repTab["models/SmallBridge/SBcorridorE2/sbcorridore2.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE2.mdl"
-repTab["models/SmallBridge/SBcorridorE3/sbcorridore3.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE3.mdl"
-repTab["models/SmallBridge/SBcorridorE4/sbcorridore4.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE4.mdl"
-repTab["models/SmallBridge/SBcorridorE05/sbcorridore05.mdl"] = "models/SmallBridge/Hulls,SW/SBhullE05.mdl"
-repTab["models/SmallBridge/SBcorridorEdh/sbcorridoredh.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh.mdl"
-repTab["models/SmallBridge/SBcorridorEdh2/sbcorridoredh2.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh2.mdl"
-repTab["models/SmallBridge/SBcorridorEdh3/sbcorridoredh3.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh3.mdl"
-repTab["models/SmallBridge/SBcorridorEdh4/sbcorridoredh4.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEdh4.mdl"
-repTab["models/SmallBridge/SBcorridorEFlip/sbcorridoreflip.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEFlip.mdl"
-repTab["models/SmallBridge/SBcorridorEnd/sbcorridorend.mdl"] = "models/SmallBridge/Hulls,SW/SBhullEnd.mdl"
-repTab["models/SmallBridge/SBcorridorR/sbcorridorr.mdl"] = "models/SmallBridge/Hulls,SW/SBhullR.mdl"
-repTab["models/SmallBridge/SBcorridorRtri/sbcorridorrtri.mdl"] = "models/SmallBridge/Hulls,SW/SBhullRtri.mdl"
-repTab["models/SmallBridge/SBcorridorSlanthalfL/sbcorridorslanthalfl.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlanthalfR.mdl"
-repTab["models/SmallBridge/SBcorridorSlanthalfR/sbcorridorslanthalfr.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlanthalfL.mdl"
-repTab["models/SmallBridge/SBcorridorSlantL/sbcorridorslantl.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlantR.mdl"
-repTab["models/SmallBridge/SBcorridorSlantR/sbcorridorslantr.mdl"] = "models/SmallBridge/Hulls,SW/SBhullSlantL.mdl"
-repTab["models/SmallBridge/SBcorridorT/sbcorridort.mdl"] = "models/SmallBridge/Hulls,SW/SBhullT.mdl"
-repTab["models/SmallBridge/SBcorridorTdl/sbcorridortdl.mdl"] = "models/SmallBridge/Hulls,SW/SBhullTdl.mdl"
-repTab["models/SmallBridge/SBcorridorTdldw/sbcorridortdldw.mdl"] = "models/SmallBridge/Hulls,SW/SBhullTdldw.mdl"
-repTab["models/SmallBridge/SBtriangleE1/sbtrianglee1.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri1.mdl"
-repTab["models/SmallBridge/SBtriangleE2/sbtrianglee2.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri2.mdl"
-repTab["models/SmallBridge/SBtriangleE3/sbtrianglee3.mdl"] = "models/SmallBridge/Hulls,SW/SBhulltri3.mdl"
-repTab["models/SmallBridge/SBcorridorX/sbcorridorx.mdl"] = "models/SmallBridge/Hulls,SW/SBhullX.mdl"
-repTab["models/SmallBridge/SBcorridorXdl/sbcorridorxdl.mdl"] = "models/SmallBridge/Hulls,SW/SBhullXdl.mdl"
-repTab["models/SmallBridge/SBcorridorXdldw/sbcorridorxdldw.mdl"] = "models/SmallBridge/Hulls,SW/SBhullXdldw.mdl"
---Smallbridge Walkways--
-repTab["models/SmallBridge/SBwalkwayE/sbwalkwaye.mdl"] = "models/SmallBridge/Walkways/SBwalkwayE.mdl"
-repTab["models/SmallBridge/SBwalkwayE2/sbwalkwaye2.mdl"] = "models/SmallBridge/Walkways/SBwalkwayE2.mdl"
-repTab["models/SmallBridge/SBwalkwayR/sbwalkwayr.mdl"] = "models/SmallBridge/Walkways/SBwalkwayR.mdl"
-repTab["models/SmallBridge/SBwalkwayT/sbwalkwayt.mdl"] = "models/SmallBridge/Walkways/SBwalkwayT.mdl"
-repTab["models/SmallBridge/SBwalkwayX/sbwalkwayx.mdl"] = "models/SmallBridge/Walkways/SBwalkwayX.mdl"
---Smallbridge Vehicles--
-repTab["models/SmallBridge/SBchair/sbchair.mdl"] = "models/SmallBridge/Vehicles/SBVPchair.mdl"
-repTab["models/SmallBridge/SBdroppod1/sbdroppod1.mdl"] = "models/SmallBridge/Vehicles/SBVdroppod1.mdl"
---Smallbridge Other Parts--
-repTab["models/SmallBridge/SBconsole1s/sbconsole1s.mdl"] = "models/SmallBridge/Other/SBconsole.mdl"
-repTab["models/SmallBridge/SBconsole1low/sbconsole1low.mdl"] = "models/SmallBridge/Other/SBconsolelow.mdl"
-repTab["models/SmallBridge/SBconsoletop1/sbconsoletop1.mdl"] = "models/SmallBridge/Other/SBconsoletop.mdl"
---Smallbridge SENTs/Animated--
-repTab["models/SmallBridge/SBdooranim1/sbdooranim1.mdl"] = "models/SmallBridge/SEnts/SBADoor1.mdl"
-repTab["models/SmallBridge/SBdooranim2/sbdooranim2.mdl"] = "models/SmallBridge/SEnts/SBADoor2.mdl"
-repTab["models/SmallBridge/sbdooranimwide.mdl"] = "models/SmallBridge/SEnts/SBADoorWide.mdl"
---Smallbridge Panels/Doors--
-repTab["models/SmallBridge/SBdoor/sbdoor.mdl"] = "models/SmallBridge/Panels/SBdoor.mdl"
-repTab["models/SmallBridge/SBdoorsquare/sbdoorsquare.mdl"] = "models/SmallBridge/Panels/SBdoorSquare.mdl"
-repTab["models/SmallBridge/sbdooriris.mdl"] = "models/SmallBridge/Panels/SBdooriris.mdl"
-repTab["models/SmallBridge/SBdoorwide/sbdoorwide.mdl"] = "models/SmallBridge/Panels/SBdoorWide.mdl"
-repTab["models/SmallBridge/SBpanelSolidg/sbpanelsolidg.mdl"] = "models/SmallBridge/Panels/SBpanelSolid.mdl"
-repTab["models/SmallBridge/SBpanelSolidgdw/sbpanelsolidgdw.mdl"] = "models/SmallBridge/Panels/SBpanelSoliddw.mdl"
-repTab["models/SmallBridge/SBpanelDH/sbpaneldh.mdl"] = "models/SmallBridge/Panels/SBpanelDH.mdl"
-repTab["models/SmallBridge/SBpanelDHDW/sbpaneldhdw.mdl"] = "models/SmallBridge/Panels/SBpanelDHDW.mdl"
-repTab["models/SmallBridge/SBpanelDoor/sbpaneldoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoor.mdl"
-repTab["models/SmallBridge/SBpanelDoordw/sbpaneldoordw.mdl"] = "models/SmallBridge/Panels/SBpanelDoordw.mdl"
-repTab["models/SmallBridge/SBpanelDoor2dw/sbpaneldoor2dw.mdl"] = "models/SmallBridge/Panels/SBpanelDoordw2.mdl"
-repTab["models/SmallBridge/SBpanelSquareDoor/sbpanelsquaredoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoorSquare.mdl"
-repTab["models/SmallBridge/SBpanelSquareDoordw/sbpanelsquaredoordw.mdl"] = "models/SmallBridge/Panels/SBpanelDoorSquaredw.mdl"
-repTab["models/SmallBridge/sbdooriris2.mdl"] = "models/SmallBridge/Panels/SBpanelDoorIris.mdl"
-repTab["models/SmallBridge/SBpanelwideDoor/sbpanelwidedoor.mdl"] = "models/SmallBridge/Panels/SBpanelDoorWide.mdl"
-repTab["models/SmallBridge/SBpanelSquareDoordockin/sbpanelsquaredoordockin.mdl"] = "models/SmallBridge/Panels/SBpanelDockIn.mdl"
-repTab["models/SmallBridge/SBpanelSquareDoordocko/sbpanelsquaredoordocko.mdl"] = "models/SmallBridge/Panels/SBpanelDockOut.mdl"
-SBEP_ReplaceTable = repTab
-SBEP.ReplaceTable = repTab
+--fill table from file
+--not run on execution as you don't always need to fix dupes
+--is a console command in case you made changes and want to try it real-time
+function SBEP_LoadReplaceTable()
+	print("Loading Filename Changes")
+	local repTab = {}
+	local tableString = file.Read("../Smallbridge Filename Changes.txt")
+	local tableRows = string.Explode("\n",tableString)
+	for _,row in pairs(tableRows) do
+		--returns the pair of strings matched by this pattern
+		--currently only finds models, if we change entity names as well this will need changing
+		old,new = string.match(row, "^[ \t]*(models/[0-9A-Za-z/,]*.mdl)[ \t]*\|[ \t]*(models/[0-9A-Za-z/,]*.mdl)[ \t]*$")
+		if (old and new) then
+			repTab[old] = new
+		end
+	end
+	SBEP_ReplaceTable = repTab
+	SBEP.ReplaceTable = repTab
+end
+SBEP.LoadReplaceTable = SBEP_LoadReplaceTable
+concommand.Add("SBEP_LoadReplaceTable",SBEP.LoadReplaceTable)
 
+--replaces all instances of models in the replace table with 
 function SBEP_FixDupe(player,command,args)
+	--if the replace table hasn't been made yet, remake it
+	if not SBEP.ReplaceTable then
+		SBEP.LoadReplaceTable()
+	end
+	--print("FixDupe called")
 	local filePath = args[1]
+	--print("File Path = ",filePath)
 	local fileString = file.Read(filePath)
-	for old,new in pairs(repTab) do
+	for old,new in pairs(SBEP.ReplaceTable) do
+		--print("Replacing "..old.." with "..new)
 		fileString = string.Replace(fileString,old,new)
 	end
 	file.Write(filePath,fileString)
 end
 SBEP.FixDupe = SBEP_FixDupe
+concommand.Add("SBEP_FixDupe",SBEP.FixDupe)
 
-function SBEP_FixAllDupes(player,command,args)
-	for _,dirPath in pairs(file.FindDir("adv_duplicator/*") ) do
-		for _,filePath in pairs(file.Find(dirPath.."/*")) do
-			SBEP.FixDupe(player,command,filePath)
+function SBEP_RecursiveFix(player,command,args)
+	--print("Recursive Fix Called")
+	local dir = args[1]
+	--print("Directories Found: ")
+	local dirs = file.FindDir(dir.."/*")
+	--PrintTable(dirs)
+	for _,dirPath in pairs(dirs) do
+		SBEP_RecursiveFix(player,command,{dirPath})
+	end
+	--print("Files Found: ")
+	local files = file.Find(dir.."/*")
+	--PrintTable(files)
+	for _,filePath in pairs(files) do
+		if (string.GetExtensionFromFilename(filePath) == "txt") then
+			SBEP.FixDupe(player,command,{dir.."/"..filePath})
 		end
 	end
 end
-SBEP.FixAllDupes = SBEP_FixAllDupes
+SBEP.RecursiveFix = SBEP_RecursiveFix
+concommand.Add("SBEP_FixDupeFolder",SBEP.RecursiveFix)
 
-concommand.Add("SBEP_FixDupe",SBEP.FixDupe)
+function SBEP_FixAllDupes(player,command)
+	--print("Fix Dupes Called")
+	SBEP.RecursiveFix(player,command,{"adv_duplicator"})
+end
+SBEP.FixAllDupes = SBEP_FixAllDupes
 concommand.Add("SBEP_FixAllDupes",SBEP.FixAllDupes)
