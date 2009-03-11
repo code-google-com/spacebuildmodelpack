ba85c5b008dccfdcaa40d72ae8a7bd6ee93ad7da
unintendedhysteria <unintendedhysteria@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-11 02:34:06 +0000

    Added the DL CRamp and Hull Cache models.

--- SpacebuildModelPackAlpha2/lua/rd2/stools/sbmp_res_mods/base_res_module.lua
+++ SpacebuildModelPackAlpha2/lua/rd2/stools/sbmp_res_mods/base_res_module.lua
@@ -7,68 +7,73 @@ DEVICEGROUP.type			= "base_default_res_module"
 ]]
 
 DEVICEGROUP.devices = {
-	add_one = {
+	sbep_res_1 = {
 		Name		= "CargoCrate",
 		model		= "models/Spacebuild/cargo1.mdl",
 	},
-	add_two = {
+	sbep_res_2 = {
 		Name		= "Tiny Cache",
 		model		= "models/Spacebuild/resourcecachetiny.mdl",
 	},
-	add_three = {
+	sbep_res_3 = {
 		Name		= "Small Cache",
 		model		= "models/Spacebuild/resourcecachesmall.mdl",
 	},
-	add_four = {
+	sbep_res_4 = {
 		Name		= "Medium Cache",
 		model		= "models/Spacebuild/resourcecachemedium.mdl",
 	},
-	add_five = {
+	sbep_res_5 = {
 		Name		= "Large Cache",
 		model		= "models/Spacebuild/resourcecachelarge.mdl",
 	},
-	add_six = {
+	sbep_res_6 = {
 		Name		= "Huge Cache",
 		model		= "models/Spacebuild/resourcecachehuge.mdl",
 	},
-	add_seven = {
+	sbep_res_7 = {
 		Name		= "Massive Cache",
 		model		= "models/Spacebuild/resourcecachemassive.mdl",
 	},
-	add_eigth = {
+	sbep_res_8 = {
 		Name		= "Modular Unit X-01",
 		model		= "models/Spacebuild/milcock4_multipod1.mdl",
 	},
-	add_nine = {
+	sbep_res_9 = {
 		Name		= "Slyfo Tank 1",
 		model		= "models/Slyfo/t-eng.mdl",
 	},
-	add_ten = {
+	sbep_res_10 = {
 		Name		= "Slyfo Power Crystal",
 		model		= "models/Slyfo/powercrystal.mdl",
 	},
-	add_11 = {
+	sbep_res_11 = {
 		Name		= "SmallBridge, Small Wall Cache",
 		model		= "models/SmallBridge/Life Support/SBwallcacheS.mdl",
 	},
-	add_12 = {
+	sbep_res_12 = {
 		Name		= "SmallBridge, Large Wall Cache",
 		model		= "models/SmallBridge/Life Support/SBwallcacheL.mdl",
 	},
 	
-	add_13 = {
+	sbep_res_13 = {
 		Name		= "SmallBridge, External Wall Cache",
 		model		= "models/SmallBridge/Life Support/SBwallcacheE.mdl",
 	},
 	
-	add_14 = {
+	sbep_res_14 = {
 		Name		= "SmallBridge, Small Wall Cache (half length)",
 		model		= "models/SmallBridge/Life Support/SBwallcacheS05.mdl",
 	},
 	
-	add_15 = {
+	sbep_res_15 = {
 		Name		= "SmallBridge, Large Wall Cache (half length)",
 		model		= "models/SmallBridge/Life Support/SBwallcacheL05.mdl",
+	},
+	
+	sbep_res_16 = {
+		Name		= "SmallBridge, Hull Cache",
+		model		= "models/SmallBridge/Life Support/SBhullcache.mdl",
 	},
 }
 
