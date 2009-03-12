83a9e7b0b85aec45ab5123353f78747bddfffa38
asphid.jackal <asphid.jackal@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-12 05:16:32 +0000

    CupNoodles, BeamCannon SEntified

---
+++ SpacebuildModelPackAlpha2/lua/entities/sbep_beam_cannon/init.lua
@@ -0,0 +1,119 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("entities/sbep_beam_cannon/sh_configs.lua")
+include('shared.lua')
+
+function ENT:SpawnFunction(ply, tr)
+	if not tr.Hit then return end
+	
+	local ent = ents.Create("sbep_beam_cannon")
+	ent:SetPos(tr.HitPos + (tr.HitNormal * 32))
+	ent:Spawn()
+	ent:Activate()
+	
+	return ent
+end
+
+function ENT:OnInit()
+	self.ConfigurationIndex = -1
+	
+	self:SetNWInt("ConfigIdx", -1)
+	
+	self.TraceSettings = {}
+	self.TraceSettings.mask   = MASK_PLAYERSOLID
+	self.TraceSettings.filter = self.Entity
+	self.Inputs = Wire_CreateInputs(self.Entity, {"Config","Fire"})
+end
+
+function ENT:TriggerInput(iname, value)
+	if(iname == "Config") then
+		if(value == 2 || value == 3 || value == 4 || value == 5) then
+			beam_config_index = value
+			self.ConfigurationIndex = value
+			self:SetNWInt("ConfigIdx", value)
+		elseif(value == 0 || value == 1) then
+			beam_config_index = -1
+			self.ConfigurationIndex = -1
+			self:SetNWInt("ConfigIdx", -1)
+		end
+	end
+
+	if (iname == "Fire") and (value == 1) then
+		return self:StartFiring()
+	elseif (iname == "Fire") and (value == 0) and self.IsToggleable then
+		return self:StopFiring()
+	end
+end
+
+function ENT:Setup(beam_config_type)
+	self.ConfigurationIndex = beam_config_type
+	
+	self:SetNWInt("ConfigIdx", beam_config_type)
+end
+
+function ENT:OnRemove()
+	if Dev_Unlink_All then return Dev_Unlink_All(self.Entity) end
+end
+
+function ENT:OnKillEnt(ent)
+	if not (ent and ent.IsValid and ent:IsValid()) then return end
+	
+	local rag
+	
+	if ent:IsVehicle() then
+		local ply = ent:GetDriver()
+		
+		if ply and ply:IsValid() then
+			ply:Kill()
+			
+			local ragb = ply:GetRagdollEntity()
+			
+			if ragb and ragb:IsValid() then
+				self:OnKillEnt(ragb)
+			end
+		end
+	elseif ent:IsPlayer() then
+		ent:Kill()
+		
+		local ragb = ent:GetRagdollEntity()
+		
+		if ragb and ragb:IsValid() then
+			rag = ragb
+		end
+	elseif ent:IsNPC() then
+		ent:TakeDamage(9001, self:GetOwner(), self.Entity) -- Do fizzics damage
+	end
+	
+	ent = rag or ent
+	
+	constraint.RemoveAll(ent)
+	
+	ent:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+	
+	local phys = ent:GetPhysicsObject()
+	
+	if phys and phys.IsValid and phys:IsValid() then
+		phys:EnableMotion(true)
+		phys:ApplyForceOffset(self:GetForward() * -phys:GetMass() * self.Configs[self.ConfigurationIndex].Damage, self.HackHitPos)
+	end
+	
+	local fx = EffectData()
+	fx:SetEntity(ent)
+	fx:SetScale(6)
+	util.Effect("sbmp_disintergrate", fx)
+	
+	SafeRemoveEntityDelayed(ent, 6)
+end
+
+function ENT:FireCannon()
+	local StartPos, EndPos, tr = self:GetFiringData()
+	
+	if tr.HitWorld then return end
+	if not tr.Entity then return end
+	
+	local damage = (tr.Entity:IsPlayer() or tr.Entity:IsNPC()) and 9001 or self.Configs[self.ConfigurationIndex].Damage
+	
+	self.HackHitPos = tr.HitPos
+	--print("Damage: ", damage)
+	self:DamageEntity(tr.Entity, damage)
+end
---
+++ SpacebuildModelPackAlpha2/lua/entities/sbep_beam_cannon/shared.lua
@@ -0,0 +1,198 @@
+-- THIS IS A HACKZED WEAPON! DO NOT USE IT FOR LEARNING!!!
+include("entities/sbep_beam_cannon/sh_configs.lua")
+
+ENT.Type 			= "anim"
+ENT.Base 			= "base_sbmp_weapon"
+
+ENT.PrintName		= "Beam Cannon"
+ENT.Author			= "Olivier 'LuaPineapple' Hamel, Asphid_Jackal"
+ENT.Contact			= "evilpineapple@cox.net"
+ENT.Purpose			= "Blow your fellow players to bits!"
+ENT.Instructions	= "Spawn, fire!"
+
+ENT.Spawnable       = false
+ENT.AdminSpawnable  = true
+
+ENT.Category		= "SBEP-Weapons"
+
+ENT.Model        = "models/Spacebuild/emount4_fighter.mdl"
+ENT.MassOverride = 750
+
+ENT.IsCoherentBeamWeapon = true
+ENT.IsToggleable = false
+
+ENT.UsesAmmoRounds = false
+
+ENT.ResList = {}
+--[[
+ENT.ResList.energy = {}
+ENT.ResList.energy.Default     = 0
+ENT.ResList.energy.Capacity    = 0
+ENT.ResList.energy.CostPerShot = ??
+--]]
+
+function ENT:OnStartFiring(timing_offset) -- To allow compensation for network latency
+	if self.ConfigurationIndex == -1 then return Error("No configuration given.\n") end -- WTF?
+	--print("firing")
+	if SERVER then
+		-- Do an inital range check so we can make the slash scale nicely
+		-- Not so elegant
+		
+		local tr = util.QuickTrace(self:GetPos(), self:GetForward() * -99999, self.Entity)
+		--[[print(type(VectorRand()))]]
+		local VecRnd = VectorRand() * self.Configs[self.ConfigurationIndex].SlashCone
+		
+		if tr.HitPos:Distance(self:GetPos()) < 512 then
+			tr.HitPos = self:GetPos() + self:GetForward() * -512 -- Hackz
+		end
+		
+		self.SlashStart = self:WorldToLocal(tr.HitPos + VecRnd):Normalize()
+		self.SlashEnd   = self:WorldToLocal(tr.HitPos + (VecRnd * -1)):Normalize()
+		--print("Setting NW timestamp")
+		self:SetNWInt("Firing", CurTime())
+		--print("GetNWInt, Firing: ", self:GetNWInt("Firing"))
+		self:SetNWVector("SlashStart", self.SlashStart) -- Use the NW stuff because the system/entity might have shifted by the time we get the "OMGZ! W3 h@v3 FI@HD!" message
+		self:SetNWVector("SlashEnd",   self.SlashEnd)
+		--print("moving to on init fire")
+		return self:OnInitFire()
+	else
+		return self:OnInitFire(timing_offset)
+	end
+end
+
+function ENT:OnInitFire(timing_offset)
+	timing_offset = timing_offset or 0
+	
+	if CLIENT then
+		self.ConfigurationIndex = self:GetNWInt("ConfigIdx")
+		
+		if self.ConfigurationIndex == -1 then return Error("Received invalid networked configuration index.\n") end -- WTF?
+		
+		--print("Configuration Index: ", self.ConfigurationIndex)
+		
+		self.SlashStart = self:GetNWVector("SlashStart")
+		self.SlashEnd   = self:GetNWVector("SlashEnd")
+		
+		local fx = EffectData()
+		fx:SetEntity(self.Entity)
+		fx:SetMagnitude(timing_offset)
+		util.Effect("sbmp_fs2_beam", fx)
+	
+		self.PredictedFireStart = self.FireTimestamp + self.Configs[self.ConfigurationIndex].EventData.OnInitFire.ProgressionDelay
+	else
+		self.PredictedFireStart = CurTime() + self.Configs[self.ConfigurationIndex].EventData.OnInitFire.ProgressionDelay
+	end
+	
+	self.PredictedFireEnd = self.PredictedFireStart + self.Configs[self.ConfigurationIndex].EventData.OnFire.ProgressionDelay + self.Configs[self.ConfigurationIndex].EventData.OnFireLoop.ProgressionDelay
+	
+	return timer.Simple(timing_offset + self.Configs[self.ConfigurationIndex].EventData.OnInitFire.ProgressionDelay, self.OnFire, self)
+end
+
+function ENT:OnFire()
+	if not (self and self:IsValid()) then return end
+	
+	self.FireTime = self.Configs[self.ConfigurationIndex].EventData.OnFire.ProgressionDelay + self.Configs[self.ConfigurationIndex].EventData.OnFireLoop.ProgressionDelay
+	self.PredictedFireEnd = CurTime() + self.FireTime
+	
+	self.FiringActive = true
+	
+	if SERVER then
+		self:DoFire()
+	end
+	
+	return timer.Simple(self.Configs[self.ConfigurationIndex].EventData.OnFire.ProgressionDelay + self.Configs[self.ConfigurationIndex].EventData.OnFireLoop.ProgressionDelay, self.OnEndFire, self)
+end
+
+function ENT:OnEndFire()
+	if not (self and self:IsValid()) then return end
+	
+	self.FiringActive = false
+	
+	timer.Simple(self.Configs[self.ConfigurationIndex].Cooldown + self.Configs[self.ConfigurationIndex].EventData.OnFireEnd.ProgressionDelay, self.ResetFiring, self)
+end
+
+function ENT:ResetFiring()
+	if not (self and self:IsValid()) then return end
+	
+	self.Firing = false -- This is more elegent then using a timestamp
+end
+
+function ENT:DoFire()
+	if not (self and self:IsValid()) then return end
+	
+	if self.FiringActive then
+		self:FireCannon()
+		
+		return timer.Simple(.2, self.DoFire, self)
+	end
+end
+
+if CLIENT then
+	function ENT:Think()
+		local firing_timestamp = self:GetNWInt("Firing")
+		
+		if firing_timestamp == 0 then return end
+		
+		if firing_timestamp ~= self.FireTimestamp then
+			--print("Timestamp differential, received ", firing_timestamp, " at ", CurTime())
+			self.FireTimestamp = firing_timestamp
+			self.Firing = true
+			
+			return self:OnStartFiring(firing_timestamp - CurTime()) -- Offsets it back to when the server fired
+		end
+	end
+end
+
+function ENT:GetFiringData()
+	local CTime = CurTime()
+	
+	local selfpos  = self:GetPos()
+	local tr_data  = nil
+	
+	local StartPos = selfpos + self:GetForward() * -45
+	local EndPos   = nil
+	
+	--print("slash start: ", self.SlashStart)
+	--print("slash end: ", self.SlashEnd)
+	
+	if self.FiringActive then
+		local perc = (self.PredictedFireEnd - CTime) / self.FireTime
+		--print(perc)
+		
+		self.TraceSettings.start  = StartPos
+		self.TraceSettings.endpos = StartPos + self:LocalToWorld(LerpVector(perc, self.SlashStart, self.SlashEnd) * 99999)
+		--self.TraceSettings.mask = MASK_PLAYERSOLID
+		self.TraceSettings.filter = self.Entity
+		
+		if SERVER then -- Jitter the beam so it seems 'fuller' and not just a thin line
+			local jitter_vec = VectorRand() * 4
+			self.TraceSettings.start  = self.TraceSettings.start + jitter_vec * .2
+			self.TraceSettings.endpos = self.TraceSettings.endpos + jitter_vec
+		end
+		
+		--print(self.TraceSettings.endpos.x, self.TraceSettings.endpos.y, self.TraceSettings.endpos.z)
+		tr_data = util.TraceLine(self.TraceSettings)
+		
+		EndPos  = tr_data.HitPos
+		
+		if CLIENT then
+			util.Decal("Scorch", EndPos + tr_data.HitNormal, EndPos - tr_data.HitNormal) 
+		end
+	else
+		self.TraceSettings.start  = StartPos
+		self.TraceSettings.endpos = StartPos + self:GetForward() * -9999
+		
+		tr_data = util.TraceLine(self.TraceSettings)
+		
+		EndPos = tr_data.HitPos
+	end
+	
+	--[[
+	local fx_debugger = EffectData()
+	fx_debugger:SetStart(StartPos)
+	fx_debugger:SetOrigin(EndPos)
+	util.Effect("sbmp_trace_debugger", fx_debugger, true, true)
+	--]]
+	
+	return StartPos, EndPos, tr_data
+end
