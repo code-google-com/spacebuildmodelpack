acab1ee6af269f8429b4cd2ca21a43f7afe82be8
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-13 20:02:09 +0000

    Added Stingray, Wraith and Shuttles to fighter tables, now you can dock. Wraith is too huge to dock at anything but landing pads.

--- SpacebuildModelPackAlpha2/lua/autorun/sh_fighters.lua
+++ SpacebuildModelPackAlpha2/lua/autorun/sh_fighters.lua
@@ -4,10 +4,12 @@ AngOff is a rotational offset, use it if the vehicle faces up or right
 Docklist is a list of the Hangars the ship can dock at
 all names lower case because the GetName() function returns lowercase sometimes
 ]]
---[[The table heirachy has been designed so that you decide what the largest ship you'll allow
+--[[The table hierarchy has been designed so that you decide what the largest ship you'll allow
 into the hangar is and all the smaller classes will also be able to dock at it.
 Semi-counter-intuitively this means that the small table has the most entries.]]
-local heavy = {"swordhangarspacious","deck","deckdouble","deckdoublewide","landingpad"}
+local huge = {"deck","deckdouble","deckdoublewide","landingpad"}
+local heavy = {"swordhangarspacious"}
+table.Add(heavy,huge)
 local large = {"dockingclamp", "dockingclampt", "dockingclampx","mbhangarside2"}
 table.Add(large,heavy)
 local medium = {"swordhangar", "swordhangarlarge","swordhangarsingle"}
@@ -16,6 +18,7 @@ local small = {"sbhangar","sbfighterbay1","sbfighterbay2","sbfighterbay3","sbfig
 table.Add(small,medium)
 local tiny = {}
 table.Add(tiny,small)
+list.Set("sbepfighters","wraith",			{VecOff=Vector(-4,0,-92), 	AngOff=Angle(0,0,0), Docklist=huge})
 list.Set("sbepfighters","cratemover",		{VecOff=Vector(0,0,145), 	AngOff=Angle(0,0,0), Docklist=heavy})
 list.Set("sbepfighters","largetransport",	{VecOff=Vector(0,0,105), 	AngOff=Angle(0,0,0), Docklist=large})
 list.Set("sbepfighters","sai",				{VecOff=Vector(-21,0,13), 	AngOff=Angle(0,0,0), Docklist=large})
@@ -28,7 +31,13 @@ list.Set("sbepfighters","knife",			{VecOff=Vector(-75,0,15), 	AngOff=Angle(0,0,0
 list.Set("sbepfighters","sawblade",			{VecOff=Vector(-96,0,0), 	AngOff=Angle(0,0,0), Docklist=small})
 list.Set("sbepfighters","arwing",			{VecOff=Vector(0,0,40), 	AngOff=Angle(0,0,0), Docklist=small})
 list.Set("sbepfighters","clunker",			{VecOff=Vector(0,0,60), 	AngOff=Angle(0,0,0), Docklist=small})
+list.Set("sbepfighters","stingray",			{VecOff=Vector(-62,0,24),	AngOff=Angle(0,0,0), Docklist=small})
 list.Set("sbepfighters","shuttle",			{VecOff=Vector(0,0,0),	 	AngOff=Angle(0,0,0), Docklist=small})
+list.Set("sbepfighters","shuttle2",			{VecOff=Vector(-67.5,0,32),	AngOff=Angle(0,0,0), Docklist=small})
+list.Set("sbepfighters","shuttle3",			{VecOff=Vector(0,0,0),	 	AngOff=Angle(0,0,0), Docklist=small})
+list.Set("sbepfighters","shuttle4",			{VecOff=Vector(0,0,0),	 	AngOff=Angle(0,0,0), Docklist=small})
 list.Set("sbepfighters","assaultpodc",		{VecOff=Vector(0,0,0),		AngOff=Angle(0,0,0), Docklist=tiny})
 list.Set("sbepfighters","dagger",			{VecOff=Vector(23,0,0),		AngOff=Angle(0,0,0), Docklist=tiny})
 list.Set("sbepfighters","dart",				{VecOff=Vector(16,0,0),		AngOff=Angle(0,0,0), Docklist=tiny})
+
+
