37e5a2d543d2448b709c1a979385d1dd83a284df
asphid.jackal <asphid.jackal@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-14 15:42:24 +0000

    Fixing CupNoodles to standard format.

---
+++ SpacebuildModelPackAlpha2/lua/entities/CupNoodle/cl_init.lua
@@ -0,0 +1,5 @@
+function ENT:Draw()
+	// self.BaseClass.Draw(self)
+	self:DrawEntityOutline( 0.0 ) 			
+	self.Entity:DrawModel() 				
+end -- Draw end
--- SpacebuildModelPackAlpha2/lua/entities/CupNoodle/init.lua
+++ SpacebuildModelPackAlpha2/lua/entities/CupNoodle/init.lua
@@ -1,5 +1,3 @@
---[[if(SERVER) then
-
 function ENT:SpawnFunction( ply, tr )
 	local ent = ents.Create("CupNoodle") 			// Create the entity
 		ent:SetPos(tr.HitPos + Vector(0, 0, 20)) 	// Set it to spawn 20 units over the spot you aim at when spawning it
@@ -28,25 +26,3 @@ function ENT:Use( activator, caller )
 		activator:SetHealth( health + 25 )
 	end -- if end
 end -- Use end
-end -- Server end]]
-
---[[if(CLIENT) then
-
-function ENT:Draw()
-	// self.BaseClass.Draw(self)
-	self:DrawEntityOutline( 0.0 ) 			
-	self.Entity:DrawModel() 				
-end -- Draw end
-
-end -- client end]]
-
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-ENT.PrintName = "Cup Noodles"
-ENT.Author = "Asphid_Jackal, SLYFo"
-ENT.Contact = "nullonentry"
-ENT.Purpose = "Get some health."
-ENT.Instructions = "Eat up!" 
-ENT.Category = "SBEP - Other"
-ENT.Spawnable = true
-ENT.AdminSpawnable = true 
