ba4c5696cec70464dae09257b0036370a88a78d4
asphid.jackal <asphid.jackal@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-21 22:06:05 +0000

    Custom hangars.

---
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/sbmp_hangar_sb_custom/init.lua
@@ -0,0 +1,38 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+function ENT:Initialize()
+	self.BaseClass:Initialize(self)
+	self.Entity:SetModel( "models/SmallBridge/Hangars/sbdb3m.mdl" )
+	self.Entity:SetName("sbHangar")
+end
+
+function ENT:InitDock()
+	self.Bay = {}
+	self.Bay["Bay"] = {}
+	self.Bay["Bay"]["ship"] = nil
+	self.Bay["Bay"]["weld"] = nil
+	self.Bay["Bay"]["pos"] = Vector(0,0,0)
+	self.Bay["Bay"]["canface"] = {Angle(0,0,0), Angle(0,180,0),--[[Angle(90,90,0),Angle(90,180,0),Angle(90,270,0),Angle(90,360,0)]]}
+	self.Bay["Bay"]["pexit"] = Vector(0,0,10)
+end
+
+function ENT:SpawnFunction( ply, tr )
+
+	if ( !tr.Hit ) then return end
+	
+	local SpawnPos = tr.HitPos + tr.HitNormal * 16 + Vector(0,0,500)
+	
+	local ent = ents.Create( "sbmp_hangar_sb_custom" )
+	ent:SetPos( SpawnPos )
+	ent:Spawn()
+	ent:Initialize()
+	ent:Activate()
+	ent:InitDock()
+	ent.SPL = ply
+	
+	return ent
+	
+end
---
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/sbmp_hangar_sb_custom/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_hangar"
+ENT.PrintName		= "Custom Smallbridge Hangar"
+ENT.Author			= "Asphid Jackal + Hysteria"
+ENT.Category		= "SBEP - Hangars"
+
+ENT.Spawnable		= true
+ENT.AdminSpawnable	= true
