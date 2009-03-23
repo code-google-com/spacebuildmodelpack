ec855baf634364b7f95751116c5dc79096d469f6
asphid.jackal <asphid.jackal@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-23 03:18:53 +0000

    Possibly fixing a screwup

--- SpacebuildModelPackAlpha2/lua/entities/WarpDrive/cl_init.lua
+++ SpacebuildModelPackAlpha2/lua/entities/WarpDrive/cl_init.lua
@@ -3,8 +3,12 @@ include('shared.lua')
 language.Add( "Cleanup_warpdrive", "Warp Drive" )
 language.Add( "Cleaned_warpdrive", "Cleaned up Warp Drive" )
 
---[[function ENT:Draw()
+function ENT:Draw()
    // self.BaseClass.Draw(self)
    self:DrawEntityOutline( 0.0 ) 			
    self.Entity:DrawModel() 					
-end]]
+end
+
+function ENT:DrawEntityOutline()
+return
+end
