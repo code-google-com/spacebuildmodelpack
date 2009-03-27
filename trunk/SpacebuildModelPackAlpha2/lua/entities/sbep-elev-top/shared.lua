b4bb7ac05c7001dddcb8a00db441e54ed29b3f11
asphid.jackal <asphid.jackal@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-03-27 09:52:57 +0000

    Elevators.  Middle level is NON-Spawnable til I work out some issues.

---
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/sbep-elev-top/shared.lua
@@ -0,0 +1,15 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+ENT.PrintName		= "Top"
+ENT.Author			= "Asphid Jackal + SlyFo"
+ENT.Category		= "SBEP-Elevators"
+
+ENT.Spawnable		= true
+ENT.AdminSpawnable	= true
+ENT.Owner			= nil
+ENT.SPL				= nil
+ENT.HPType			= "TLevel"
+ENT.APPos			= Vector(0,0,65)
+ENT.APAng			= Angle(0,0,0)
+ENT.FTime			= 0
+ENT.HasHardpoints	= false
