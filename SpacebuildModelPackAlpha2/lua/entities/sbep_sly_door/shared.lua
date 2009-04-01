be71413ff6b1ef526db4073490fbdece0f6ba4b2
unintendedhysteria <unintendedhysteria@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-04-01 22:35:33 +0000

    Added the slydoor code.

---
+++ SpacebuildModelPackAlpha2/lua/entities/sbep_sly_door/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type			      = "anim"
+ENT.Base                  = "sbep_base_door" 
+ENT.PrintName		      = "Slyfo Blast Door"
+ENT.Author			      = "Slyfo + Hysteria"
+ENT.Category			  = "SBEP - Other"
+
+ENT.AutomaticFrameAdvance = true
+ENT.Spawnable			  = true
+ENT.AdminSpawnable		  = true
+
+ENT.Purpose 		      = "To open and close."
