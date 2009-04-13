fb5143d009b720bc12dbbe93f94f05601f929626
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-04-13 23:00:51 +0000

    Added fighter zoom controls to the manual and made it have a default value for if it's used incorrectly. Added Wire output to doors giving its open status by request.

--- trunk/SpacebuildModelPackAlpha2/lua/autorun/client/vehviewhook.lua
+++ trunk/SpacebuildModelPackAlpha2/lua/autorun/client/vehviewhook.lua
@@ -28,22 +28,30 @@ end
 local function IncrementZoom(player,command,args)
 	--[[print("Increment Args")
 	PrintTable(args)]]
+	local zoomMod = 10
+	if (#args != 0) then
+		zoomMod = args[1]
+	end
 	if not (player and player:IsValid()) then return end
 	local veh = player:GetVehicle()
 	if not (veh and veh:IsValid()) then return end
 	local cont = veh:GetNetworkedEntity("Controller")
 	if veh:GetNetworkedInt("OffsetOut") then
-		veh:SetNetworkedInt("OffsetOut",veh:GetNetworkedInt("OffsetOut") - args[1])
+		veh:SetNetworkedInt("OffsetOut",veh:GetNetworkedInt("OffsetOut") - zoomMod)
 	end
 end
 local function DecrementZoom(player,command,args)
 	--[[print("Decrement Args")
 	PrintTable(args)]]
+	local zoomMod = 10
+	if (#args != 0) then
+		zoomMod = args[1]
+	end
 	if not (player and player:IsValid()) then return end
 	local veh = player:GetVehicle()
 	if not (veh and veh:IsValid()) then return end
 	if veh:GetNetworkedInt("OffsetOut") then
-		veh:SetNetworkedInt("OffsetOut",veh:GetNetworkedInt("OffsetOut") + args[1])
+		veh:SetNetworkedInt("OffsetOut",veh:GetNetworkedInt("OffsetOut") + zoomMod)
 	end
 end
 concommand.Add("sbep_zoom_in",IncrementZoom)
