cd19f871b91fc0988b0607f4b656030192d8a354
richard.maw <richard.maw@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-04-20 07:29:59 +0000

    Advanced GyroPod v3 uploaded Version 3 Updated 4/20/09: Fixed glitchy super sensitive pitch/yaw problem. Fixed roll stabilizer being on all the time.

--- SpacebuildModelPackAlpha2/lua/weapons/gmod_tool/stools/gyropod_advanced.lua
+++ SpacebuildModelPackAlpha2/lua/weapons/gmod_tool/stools/gyropod_advanced.lua
@@ -3,13 +3,13 @@ TOOL.Name			= "#Gyro-Pod Advanced"
 TOOL.Command		= nil
 TOOL.ConfigName		= ""
 TOOL.ent = {}
-TOOL.ClientConVar[ "model" ] = "models/props_combine/headcrabcannister01a.mdl"
+TOOL.ClientConVar[ "model" ] = "models/Spacebuild/Nova/drone2.mdl"
 
 if ( CLIENT ) then
 	language.Add( "Tool_gyropod_advanced_name", "DataSchmuck's Enhanced Gyro-Pod" )
-	language.Add( "Tool_gyropod_advanced_desc", "Customize controls & sensitivity with inputs. Last vehicle linked controls direction. Reload to select model." )
-	language.Add( "Tool_gyropod_advanced_0", "Right-Click to spawn.  Left click a prop or vehicle to start linking it.  You MUST link the Gyro to every major prop." )
-	language.Add( "Tool_gyropod_advanced_1", "Now click the Gyro-Pod.  THE ERROR MESSAGE IS YOUR CONFIRMATION THAT IT LINKED CORRECTLY. Reload to cancel linking" )	
+	language.Add( "Tool_gyropod_advanced_desc", "Customize controls & sensitivity with inputs. READ THE INSTRUCTIONS!" )
+	language.Add( "Tool_gyropod_advanced_0", "Left-Click to spawn.  Right click the Gyro-Pod to start linking a vehicle." )
+	language.Add( "Tool_gyropod_advanced_1", "Now Right click the vehicle that will control the Gyro-Pod via mouselook.  Reload to cancel." )	
 	language.Add( "Tool_turret_type", "Type of weapon" )	
 end
 
@@ -19,24 +19,29 @@ end
 cleanup.Register( "gyropod_advanceds" )	
 
 --Link an entity to the gyropod
-function TOOL:LeftClick( trace )
-if ( !trace.Hit ) then return end
-	if (self:GetStage() == 0) and (!trace.Entity.GPod) and (trace.Entity:IsValid()) then
-		self.LEnt = trace.Entity
+function TOOL:RightClick(trace)
+	if (self:GetStage() == 0) and trace.Entity:GetClass() == "gyropod_advanced" then
+		self.AdvGyro = trace.Entity
 		self:SetStage(1)
 		return true
-	elseif (self:GetStage() == 1) and (trace.Entity.GPod) and (trace.Entity:IsValid()) then
-		trace.Entity:Link(self.LEnt)
+	elseif self:GetStage() == 1 and trace.Entity.GetPassenger then
+		local owner = self:GetOwner()
+		if self.AdvGyro:Link(trace.Entity) then
+			owner:PrintMessage(HUD_PRINTTALK,"Vehicle Linked to Gyro-Pod!")
+		else
+			owner:PrintMessage(HUD_PRINTTALK,"Link failed!")
+		end
 		self:SetStage(0)
+		self.AdvGyro = nil
 		return true
 	else
+		self:GetOwner():PrintMessage(HUD_PRINTTALK,"Left Click to Spawn.  To Link, Right Click the Gyro-Pod First, THEN the Vehicle!")
 		return false
 	end
-	return true
 end
 
 --Spawn the gyropod
-function TOOL:RightClick( trace )
+function TOOL:LeftClick( trace )
 	if (!trace.HitPos) then return false end
 	if (trace.Entity:IsPlayer()) then return false end
 	if ( CLIENT ) then return true end
@@ -59,9 +64,11 @@ function TOOL:RightClick( trace )
 	return true
 end
 
---Set the gyropds model, also clear the selected ent
+--Set the gyropods model, also clear the selected ent
 function TOOL:Reload( trace )
-	self.LEnt = nil
+	if (self:GetStage()== 0) and trace.Entity:GetClass() == "gyropod_advanced" then
+		self.AdvGyro = nil
+	end	
 	if (self:GetStage()== 0) then
 		if CLIENT and trace.Entity:IsValid() then return true end
 		if not trace.Entity:IsValid() then return end
@@ -70,7 +77,7 @@ function TOOL:Reload( trace )
 		self.Model = model
 	end
 	self:SetStage(0)
-	return true;
+	return true
 end
 
 if (SERVER) then
@@ -95,31 +102,6 @@ if (SERVER) then
 	duplicator.RegisterEntityClass("gyropod_advanced", MakeDataGPod, "Model", "Pos", "Ang", "Vel", "aVel", "frozen")
 end
 
---Show the spawn ghost
---[[function TOOL:UpdateGhostDataGPod( ent, player )
-	if ( !ent || !ent:IsValid() ) then return end
-	local tr 	= utilx.GetPlayerTrace( player, player:GetCursorAimVector() )
-	local trace 	= util.TraceLine( tr )
-	if (!trace.Hit || trace.Entity:IsPlayer() || trace.Entity:GetClass() == "gyropod_advanced" ) then
-		ent:SetNoDraw( true )
-		return
-	end
-	local Ang = trace.HitNormal:Angle()
-	Ang.pitch = Ang.pitch + 90
-	local min = ent:OBBMins()
-	ent:SetPos( trace.HitPos - trace.HitNormal * min.z )
-	ent:SetAngles( Ang )
-	ent:SetNoDraw( false )
-end
-
---more ghost and model stuff
-function TOOL:Think()
-	if (!self.GhostEntity || !self.GhostEntity:IsValid() || self.GhostEntity:GetModel() != self.Model ) then
-		self:MakeGhostEntity( self:GetClientInfo("model"), Vector(0,0,0), Angle(0,0,0) )
-	end
-
-	self:UpdateGhostDataGPod( self.GhostEntity, self:GetOwner() )
-end]]
 
 function TOOL.BuildCPanel( CPanel )
 	CPanel:AddControl( "Header", { Text = "#Tool_gyropod_advanced_name", Description	= "#Tool_gyropod_advanced_desc" }  )
@@ -130,40 +112,28 @@ function TOOL.BuildCPanel(panel)
 	local BindLabel0 = {}
 	local BindLabel1 = {}
 	local BindLabel2 = {}
---[[	local BindLabel3 = {}
-	local BindLabel4 = {}
-	local BindLabel5 = {}
-	local BindLabel6 = {}
-	local BindLabel7 = {}
-	local BindLabel8 = {}
-	local BindLabel9 = {}
-	local BindLabel10 = {}
-	local BindLabel11 = {}]]
+
 	BindLabel0.Text =
 [[BASIC INSTRUCTIONS
 
-1:  Right Click to create a Gyro-Pod.
-2:  Left Click a prop.
-3:  Left Click Gyro-Pod to create Link.
-4: Repeat Steps 2 & 3 for all major props.
-5:  Left click a vehicle.
-6:  Left click Gyro-Pod to Link vehicle.
-7:  Wire the 'Activate' input to a TOGGLED output.
-8:  Wire movement controls to an Adv. Pod Controller.
-9:  Place near center of ship.
-10:  Orient the Gyro-Pod so it's aligned with ship.
-11:  Weld Gyro-pod to ship.
-12:  Enter your linked vehicle.
-13:  Press key you wired to the 'Activate' input to turn on/off.
-14:  Use MouseLook to control Pitch and Yaw.
-15:  Use the keys you wired to the movement controls to move the ship through space.
+1:   Left Click to create a Gyro-Pod.
+2:   Wire the 'Activate' input to a TOGGLED output.
+3:   Wire movement controls to an Adv. Pod Controller.
+4a:  To control Pitch/Yaw with mouse movement:
+        Right Click the gyro-pod.
+        Right Click the vehicle.
+4b:  To control Pitch/yaw with inputs:
+        DO NOT link the vehicle to the Gyro-pod.
+        Wire the PitchUp, PitchDown, YawLeft, YawRight,
+        inputs to an Adv. Pod Controller
+5:   Place Gyro-Pod near center of ship.
+6:  Orient the Gyro-Pod so it's aligned with ship.
+7:  Weld the Gyro-pod to the ship.
+8:  Press key you wired to the 'Activate' input to turn on/off.
+9:  Use the keys you wired to the movement controls to move the ship through space.
 
 ADVANCED CONTROLS (OPTIONAL)
-
-DISABLE MOUSE LOOK 
-Uses Input keys for Pitch and Yaw.
-Wire a value of 1 to the 'No Mouse' input.
-Wire Adv Pod Controller inputs or Numpad inputs to the PitchUP and Down and YawLeft and Right inputs.]]
+]]
 
 	BindLabel1.Text =
 [[MULTIPLIERS
@@ -179,22 +149,25 @@ Default is 112.  Max is 112
 
 AIMING MODE
 Points ship towards GPS coords.
-Wire AimMode to a TOGGLED output.
-Wire the Aim XYZ (or AimVec) inputs to WORLD XYZ cords.
+Wire AIMMODE to a TOGGLED output.
+Wire the AIM XYZ (or AIMVEC) inputs to WORLD XYZ cords.
 
 FREEZE SHIP
 All CONSTRAINED entities will be frozen
-Wire the Freeze Input to a TOGGLED output.
-You CAN still use your physgun's reload function to unfreeze the ship.]]
+Wire the FREEZE Input to a TOGGLED output.
+You CAN still use your physgun's reload function to unfreeze the ship.
+]]
 
 	BindLabel2.Text =
-[[TIPS
+[[AUTOLEVEL
+Keeps ship's roll and pitch at 0.
+Wire LEVEL to a TOGGLED	output.
+
+TIPS
  *  Press Reload on any prop to set the model of the Gyro-Pod to that prop.
- *  If you see an error after you try to link a prop, DON'T WORRY, it worked.
- *  You only need to Gyro-Link a few of the main parts of your ship.
- *  If you need to cancel a link, press Reload (which will also reset the model).
- *  If you PARENT the Gyro-Pod, you will lose the ability to Link it or Wire it, so save parenting for last.
- *  The Gyro-Pod's Pitch and Yaw are ONLY controlled by your mouse movement while inside a vehicle, and ONLY by the LAST vehicle you linked to the Gyro-Pod.
+ *  You do not need to gyro-link anything anymore.  Any props whos mass is over 20 will be automatically linked.  Wire components are never linked.
+ *  If you need to cancel a link, press Reload on the Gyro-Pod.
+ *  If you PARENT the Gyro-Pod, you will lose the ability to modify the wiring, so save parenting for last.
  *  Try wiring a 'Not' gate to the 'Active' Output of your pod controller, then wiring the 'Freeze' Input of the Gyro-Pod to the 'Not' gate.
  *  Joystick is supported, but not tested
  
@@ -210,30 +183,5 @@ You CAN still use your physgun's reload function to unfreeze the ship.]]
 	BindLabel2.Description = "Basic Instructions3"
 	panel:AddControl("Label", BindLabel2 )
 	
---[[	BindLabel3.Description = "Advanced"
-	panel:AddControl("Label", BindLabel3 )
-	
-	BindLabel4.Description = "NoMouse"
-	panel:AddControl("Label", BindLabel4 )
-	
-	BindLabel5.Description = "Multiplier"
-	panel:AddControl("Label", BindLabel5 )
-	
-	BindLabel6.Description = "Speed Limit"
-	panel:AddControl("Label", BindLabel6 )
-	
-	BindLabel7.Description = "Targeting Mode"
-	panel:AddControl("Label", BindLabel7 )
-	
-	BindLabel8.Description = "Freeze Mode"
-	panel:AddControl("Label", BindLabel8 )
-	
-	BindLabel9.Description = "Tips1"
-	panel:AddControl("Label", BindLabel9 )	
-	
-	BindLabel10.Description = "Tips2"
-	panel:AddControl("Label", BindLabel10 )	
-	
-	BindLabel11.Description = "Credits"
-	panel:AddControl("Label", BindLabel11 )	]]
+
 end
