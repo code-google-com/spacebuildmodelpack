eb7accaccc0b88536b98c591c0f1bb7671caa6b1
levybreak098 <levybreak098@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-05-04 22:08:20 +0000

    Committed the MMU. Experiement with it. You'll figure it out eventually.

---
+++ SpacebuildModelPackAlpha2/lua/weapons/sbep_mmu/cl_init.lua
@@ -0,0 +1,7 @@
+include('shared.lua')
+
+SWEP.PrintName			= "Experimental Manned Mobility Unit"			
+SWEP.Slot				= 5
+SWEP.SlotPos			= 2
+SWEP.DrawAmmo			= true
+SWEP.DrawCrosshair		= true 
---
+++ SpacebuildModelPackAlpha2/lua/weapons/sbep_mmu/init.lua
@@ -0,0 +1,8 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include('shared.lua')
+
+
+SWEP.Weight				= 100
+SWEP.AutoSwitchTo		= false
+SWEP.AutoSwitchFrom		= false
