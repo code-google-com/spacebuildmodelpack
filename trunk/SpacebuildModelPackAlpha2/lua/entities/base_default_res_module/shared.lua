2f7bff81237006a17bbda3aede7842ec958c668f
stijn.sv <stijn.sv@c4e7cb00-6a3d-0410-b8de-974b3ec55380>
2009-05-30 18:51:24 +0000

    Update for the changes made to RD3 in the Update pack: http://www.snakesvx.net/forum/viewtopic.php?f=22&t=71

--- trunk/SpacebuildModelPackAlpha2/lua/entities/base_default_res_module/shared.lua
+++ trunk/SpacebuildModelPackAlpha2/lua/entities/base_default_res_module/shared.lua
@@ -1,5 +1,5 @@
 ENT.Type 		= "anim"
-ENT.Base 		= "base_rd_entity"
+ENT.Base 		= "base_rd3_entity"
 ENT.PrintName 	= "Resource Module"
 
 list.Set( "LSEntOverlayText" , "base_default_res_module", {num = -1} )
